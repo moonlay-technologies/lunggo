e6b7cf874a04f7977906870938ed532f2e9f3a7b
Rama Adhitia <Rama.Adhitia@moonlay.com>
2014-03-25 16:41:21 +0700

    Initial Commit

---
+++ Lunggo.CustomerWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lunggo.CustomerWeb.MvcApplication" Language="C#" %>
---
+++ Lunggo.CustomerWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lunggo.CustomerWeb.Startup))]
+namespace Lunggo.CustomerWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
