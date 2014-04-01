4bdad3e8dd8d05c84e8afc178a639310c58082d1
Rama Adhitia <Rama.Adhitia@moonlay.com>
2014-04-01 10:00:31 +0700

    Add Lunggo.Driver Project

---
+++ Lunggo.Driver/RamaDriver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lunggo.Driver
+{
+    class RamaDriver
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
