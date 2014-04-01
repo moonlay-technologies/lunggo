423dbb4b043e5d4609a1cd71e17e3cdd8739e92b
Rama Adhitia <Rama.Adhitia@moonlay.com>
2014-04-01 10:08:58 +0700

    Add Lunggo.Framework Project

---
+++ Lunggo.Framework/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lunggo.Framework.Http
+{
+    public class Class1
+    {
+
+    }
+}
