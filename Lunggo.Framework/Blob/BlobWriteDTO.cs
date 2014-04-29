4eaf089fb3bd512722ffafc05fd7c7de2c43e283
Bayu Alvian <bayu.alvian@moonlay.com>
2014-04-29 09:01:23 +0700

    Interface Blob Storage client, singleton blob storage service

---
+++ Lunggo.Framework/Blob/BlobWriteDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lunggo.Framework.Blob
+{
+    public class BlobWriteDTO
+    {
+        public FileBlobModel FileBlobModel { get; set; }
+        private SaveMethod _saveMethod = SaveMethod.SKIP;
+        public SaveMethod SaveMethod
+        {
+            get { return _saveMethod; }
+            set { _saveMethod = value; }
+        }
+
+    }
+    public enum SaveMethod
+    {
+        FORCE,
+        SKIP,
+        EXCEPTION,
+        GENERATE_NEW_NAME
+    };
+}
--- Lunggo.Framework/Blob/FileBlobModel.cs
+++ Lunggo.Framework/Blob/FileBlobModel.cs
@@ -8,22 +8,10 @@ namespace Lunggo.Framework.Blob
 {
     public class FileBlobModel
     {
-        public string FileName { get; set; }
+        public string FilePath { get; set; }
         public string ContentType { get; set; }
         public byte[] FileByte { get; set; }
-        private SaveMethod saveMethod = SaveMethod.SKIP;
-        public SaveMethod SaveMethod
-        {
-            get { return saveMethod; }
-            set { saveMethod = value; }
-        }
 
     }
-    public enum SaveMethod
-    {
-        FORCE,
-        SKIP,
-        EXCEPTION,
-        GENERATE_NEW_NAME
-    };
+    
 }
