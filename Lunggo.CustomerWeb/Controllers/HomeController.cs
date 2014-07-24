238df68eed4b00a6d670cd2846a20bbccb70b54c
Bayu Alvian <bayu.alvian@moonlay.com>
2014-07-24 09:25:11 +0700

    add multilanguage

--- Lunggo.CustomerWeb/Controllers/HomeController.cs
+++ Lunggo.CustomerWeb/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Lunggo.Framework.Message;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,6 +11,7 @@ namespace Lunggo.CustomerWeb.Controllers
     {
         public ActionResult Index()
         {
+            string val= MessageReader.GetInstance().GetMessageValue("VAL0001", "en_US");
             return View();
         }
 
