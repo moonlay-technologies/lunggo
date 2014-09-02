be88fdb5d2ca1a401fd85361b2cc54c9128205e8
Rama Adhitia <Rama.Adhitia@moonlay.com>
2014-09-02 15:41:26 +0700

    Enable Windows Azure Diagnostics

--- Lunggo.CustomerWeb/Controllers/HomeController.cs
+++ Lunggo.CustomerWeb/Controllers/HomeController.cs
@@ -1,10 +1,5 @@
-﻿using Lunggo.ApCommon.Sequence;
-using Lunggo.Framework.Message;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using System.Diagnostics;
 
 namespace Lunggo.CustomerWeb.Controllers
 {
@@ -12,7 +7,7 @@ namespace Lunggo.CustomerWeb.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.sequence = SampleSequence.GetInstance().GetNext();
+            Trace.TraceWarning("Trace.TraceWarning");
             return View();
         }
 
