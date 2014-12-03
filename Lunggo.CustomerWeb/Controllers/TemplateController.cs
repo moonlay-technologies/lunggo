8f9323ae553f7dfdec963cb55b2327c13315c9cc
Ricky Prahatama <ricky.prahatama@travelmadezy.com>
2014-12-03 11:33:31 +0700

    Update views

---
+++ Lunggo.CustomerWeb/Controllers/TemplateController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lunggo.CustomerWeb.Controllers
+{
+    public class TemplateController : Controller
+    {
+        // GET: Template
+        public ActionResult HotelSearchList()
+        {
+            return View();
+        }
+    }
+}
