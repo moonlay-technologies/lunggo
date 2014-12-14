3792af4f53e92c516ad9fa01070b999c9b8f45ba
Rama Adhitia <rama.adhitia@travelmadezy.com>
2014-12-14 16:33:12 +0700

    1st Try Develop API

---
+++ Lunggo.ApCommon/Hotel/Logic/Search/HotelsSearchService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Lunggo.ApCommon.Hotel.Model;
+
+namespace Lunggo.ApCommon.Hotel.Logic.Search
+{
+    public class HotelsSearchService
+    {
+        public IEnumerable<String> SearchHotel(HotelsSearchRequest request)
+        {
+            return new List<String>();
+        }
+    }
+}
---
+++ Lunggo.ApCommon/Hotel/Model/HotelsSearchRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lunggo.ApCommon.Hotel.Constant;
+
+namespace Lunggo.ApCommon.Hotel.Model
+{
+    public class HotelsSearchRequest
+    {
+        public DateTime StayDate { get; set; }
+        public int StayLength { get; set; }
+        public String ResultCurrency { get; set; }
+        public int LocationId { get; set; }
+        public String Residency { get; set; }
+        public IEnumerable<RoomOccupant> RoomOccupants { get; set; }
+        public HotelSearchSortType SortBy { get; set; }
+
+    }
+}
