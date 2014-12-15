5c5c88959841414ffa5dda3f440537fd2394ad05
Rama Adhitia <rama.adhitia@travelmadezy.com>
2014-12-15 14:56:16 +0700

    Create Dummy Hotel Search Result API

--- Lunggo.WebAPI/App_Start/WebApiConfig.cs
+++ Lunggo.WebAPI/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace Lunggo.WebAPI
 {
@@ -10,6 +11,9 @@ namespace Lunggo.WebAPI
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+
+            // Enable Cors
+            config.EnableCors();
 
             // Web API routes
             config.MapHttpAttributeRoutes();
--- Lunggo.WebAPI/Controllers/HotelsController.cs
+++ Lunggo.WebAPI/Controllers/HotelsController.cs
@@ -1,30 +1,107 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace Lunggo.WebAPI.Controllers
 {
+
     public class HotelsController : ApiController
     {
-        public IEnumerable<HotelSearchResultViewModel> GetHotels([FromUri] SimpleModel model)
+        [EnableCors(origins: "http://localhost", headers: "*", methods: "*")]
+        public HotelSearchApiResponse GetHotels([FromUri] HotelSearchApiRequest request)
         {
-            /*var list = new List<String>
+            var response = new HotelSearchApiResponse
             {
-                model.StayDate, 
-                model.LocationId.ToString(CultureInfo.InvariantCulture)
-            };*/
-            return list;
+                SearchId = "dummySearchId",
+                InitialRequest = request,
+                HotelList = new List<HotelExcerpt>
+                {
+                    new HotelExcerpt
+                    {
+                        HotelName = "Hotel Borobudur",
+                        HotelId = "456789",
+                        Address = "Jalan Lapangan Banteng, Jakarta Pusat",
+                        Area = "Gambir",
+                        Country = "Indonesia",
+                        Province = "DKI Jakarta",
+                        StarRating = 5,
+                        Latitude = 67,
+                        Longitude = 115,
+                        ImageUrlList = new List<String>
+                        {
+                            "http://bestjakartahotels.com/wp-content/dubai_hotel/5-Hotel_Borobudur_Jakarta.jpg",
+                            "http://www.cleartrip.com/places/hotels//4373/437368/images/8536640_w.jpg"
+                        },
+                        LowestPrice = new Price()
+                        {
+                            Value = 500000,
+                            Currency = "IDR"
+                        }
+                    },
+                    new HotelExcerpt
+                    {
+                        HotelName = "Hotel Sultan",
+                        HotelId = "456789",
+                        Address = "Jalan Gatot Subroto, Jakarta Pusat",
+                        Area = "Senayan",
+                        Country = "Indonesia",
+                        Province = "DKI Jakarta",
+                        StarRating = 5,
+                        Latitude = 67,
+                        Longitude = 115,
+                        ImageUrlList = new List<String>
+                        {
+                            "http://images.yuktravel.com/images/upload/review/1333112196-The_Sultan_Hotel_Jakarta_Exterior.jpg",
+                            "http://data.tribunnews.com/foto/bank/images/hotel-sultan-1.jpg"
+                        },
+                        LowestPrice = new Price()
+                        {
+                            Value = 500000,
+                            Currency = "IDR"
+                        }
+                    },
+                    new HotelExcerpt
+                    {
+                        HotelName = "Hotel Sultan",
+                        HotelId = "456789",
+                        Address = "Jalan Gatot Subroto, Jakarta Pusat",
+                        Area = "Senayan",
+                        Country = "Indonesia",
+                        Province = "DKI Jakarta",
+                        StarRating = 5,
+                        Latitude = 67,
+                        Longitude = 115,
+                        ImageUrlList = new List<String>
+                        {
+                            "http://hikarivoucher.com/files/hotels/547/keraton-at-the-plaza.jpg",
+                            "http://assets.keratonattheplazajakarta.com/lps/assets/gallery/lux3635po.126266_lg.jpg"
+                        },
+                        LowestPrice = new Price()
+                        {
+                            Value = 500000,
+                            Currency = "IDR"
+                        }
+                    },
+                },
+                TotalHotelCount = 3 
+            };
+            return response;
         }
     }
 
-    public class SimpleModel
+    public class HotelSearchApiRequest
     {
         public int LocationId { get; set; }
         public String StayDate { get; set; }
+        public int StayLength { get; set; }
+        public String SearchId { get; set; }
+        public int SortBy { get; set; }
+        public int StartIndex { get; set; }
+        public int ResultCount { get; set; }
+        public long MinPrice { get; set; }
+        public long MaxPrice { get; set; }
+        public String StarRating { get; set; }
     }
 
     public class Price
@@ -33,18 +110,26 @@ namespace Lunggo.WebAPI.Controllers
         public String Currency { get; set; }
     }
 
-    public class HotelSearchResultViewModel
+    public class HotelSearchApiResponse
     {
-        public String HotelId { get; set; }
+        public IEnumerable<HotelExcerpt> HotelList { get; set; }
+        public int TotalHotelCount { get; set; }
+        public HotelSearchApiRequest InitialRequest { get; set; }
+        public String SearchId { get; set; }
+    }
+
+    public class HotelExcerpt
+    {
         public String HotelName { get; set; }
-        public String HotelDescription { get; set; }
-        public int StarRating { get; set; }
+        public String HotelId { get; set; }
         public String Address { get; set; }
+        public int StarRating { get; set; }
         public double Latitude { get; set; }
         public double Longitude { get; set; }
         public String Country { get; set; }
-        public String City { get; set; }
+        public String Province { get; set; }
         public String Area { get; set; }
-        public Price LowestRoomPrice { get; set; }
+        public List<String> ImageUrlList { get; set; }
+        public Price LowestPrice { get; set; }
     }
 }
