d23ca4af179315386322e6e287572b5a188e7f38
Rama Adhitia <rama.adhitia@travelmadezy.com>
2015-03-18 11:41:51 +0700

    Move Autocomplete controller to WebAPI project

---
+++ Lunggo.WebAPI/ApiSrc/v1/Autocomplete/AutoCompleteController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using Lunggo.ApCommon.Dictionary;
+using Lunggo.ApCommon.Model;
+
+namespace Lunggo.WebAPI.ApiSrc.v1.Autocomplete
+{
+    public class AutocompleteController : ApiController
+    {
+        [HttpGet]
+        [EnableCors(origins: "http://localhost,https://localhost,http://localhost:23321,https://localhost:23321", headers: "*", methods: "*")]
+        [Route("api/v1/autocomplete/airline")]
+        public IEnumerable<Airline> Airline(string prefix)
+        {
+            return TrieIndex.Airline.GetAllSuggestionIds(prefix).Select(id => Code.Airline[id]);
+        }
+
+        [HttpGet]
+        [EnableCors(origins: "http://localhost,https://localhost,http://localhost:23321,https://localhost:23321", headers: "*", methods: "*")]
+        [Route("api/v1/autocomplete/airport")]
+        public IEnumerable<Airport> Airport(string prefix)
+        {
+            return TrieIndex.Airport.GetAllSuggestionIds(prefix).Select(id => Code.Airport[id]);
+        }
+
+        [HttpGet]
+        [EnableCors(origins: "http://localhost,https://localhost,http://localhost:23321,https://localhost:23321", headers: "*", methods: "*")]
+        [Route("api/v1/autocomplete/hotellocation")]
+        public IEnumerable<ApCommon.Dictionary.Hotel> Hotel(string prefix)
+        {
+            return TrieIndex.Hotel.GetAllSuggestionIds(prefix).Select(id => Code.Hotel[id]);
+        }
+    }
+}
