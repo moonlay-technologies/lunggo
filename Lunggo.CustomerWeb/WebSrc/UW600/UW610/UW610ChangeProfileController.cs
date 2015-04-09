4d9a2498d920055bc491e56586d5b772fd924fbc
Bernadinus Realino Edy Gunawan <bernadinus.realino@travelmadezy.com>
2015-04-09 12:25:00 +0700

    Logic,Model,Object for Uw610

---
+++ Lunggo.CustomerWeb/WebSrc/UW600/UW610/UW610ChangeProfileController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lunggo.CustomerWeb.WebSrc.UW600.UW610.Logic;
+using Lunggo.CustomerWeb.WebSrc.UW600.UW610.Object;
+using Lunggo.CustomerWeb.WebSrc.UW600.UW610.Model;
+
+namespace Lunggo.CustomerWeb.WebSrc.UW600.UW610
+{
+    public class Uw610ChangeProfileController : Controller
+    {
+        // GET: UW610ChangeProfile
+        public ActionResult ChangeProfile(Uw610ChangeProfileRespone request)
+        {
+            Uw610ChangePofile respone = ChangeProfileLogic.SendPofile(request); 
+            return View(respone);
+        }
+    }
+}
