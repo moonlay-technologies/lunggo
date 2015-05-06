87626e17c8ede95b208e140660e547868da65325
Ricky Prahatama <ricky.prahatama@gmail.com>
2015-05-06 10:17:03 +0700

    Update Application JS

--- Lunggo.CustomerWeb/Assets/js/application.js
+++ Lunggo.CustomerWeb/Assets/js/application.js
@@ -13,37 +13,6 @@ $(document).ready(function () {
 
 });
 
-//******************************************
-// Variables
-var SystemConfig = {
-    SystemBusy: false
-};
-
-var SearchHotelConfig = {
-    Url: 'http://travorama-apidev.azurewebsites.net/api/v1/hotels',
-    ResultCount: 24
-};
-
-var SearchRoomConfig = {
-    Url: 'http://travorama-apidev.azurewebsites.net/api/v1/rooms'
-};
-
-var FlightSearchConfig = {
-    Url: 'http://dv1-api.azurewebsites.net/api/v1/flights'
-};
-
-var RevalidateConfig = {
-    Url: 'http://travorama-apidev.azurewebsites.net/api/v1/flights/revalidate',
-    working: false
-};
-
-var HotelAutocompleteConfig = {
-    Url: 'http://travorama-apidev.azurewebsites.net/api/v1/autocomplete/hotellocation/'
-};
-
-var FlightAutocompleteConfig = {
-    Url: 'http://travorama-apidev.azurewebsites.net/api/v1/autocomplete/airport/'
-};
 
 //******************************************
 // payment option form
