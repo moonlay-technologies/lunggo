872a81feb35d5e611c3096cb251c2d578457c694
Indera Aji Waskitho <indera.aji@travelmadezy.com>
2015-05-06 14:03:55 +0700

    Minor Bugfix in application.js

--- Lunggo.CustomerWeb/Assets/js/application.js
+++ Lunggo.CustomerWeb/Assets/js/application.js
@@ -14,38 +14,6 @@ $(document).ready(function () {
 });
 
 //******************************************
-// Variables
-var SystemConfig = {
-    SystemBusy: false
-};
-
-var SearchHotelConfig = {
-    Url: 'http://dv2-api.azurewebsites.net/api/v1/hotels',
-    ResultCount: 24
-};
-
-var SearchRoomConfig = {
-    Url: 'http://dv2-api.azurewebsites.net/api/v1/rooms'
-};
-
-var FlightSearchConfig = {
-    Url: 'http://dv2-api.azurewebsites.net/api/v1/flights'
-};
-
-var RevalidateConfig = {
-    Url: 'http://dv2-api.azurewebsites.net/api/v1/flights/revalidate',
-    working: false
-};
-
-var HotelAutocompleteConfig = {
-    Url: 'http://dv2-api.azurewebsites.net/api/v1/autocomplete/hotellocation/'
-};
-
-var FlightAutocompleteConfig = {
-    Url: 'http://dv2-api.azurewebsites.net/api/v1/autocomplete/airport/'
-};
-
-//******************************************
 // payment option form
 function paymentOptionForm() {
 
