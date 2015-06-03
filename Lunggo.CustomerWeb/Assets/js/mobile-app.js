017048b2b9bccf222a5dd55715f9c93477578241
Ricky Prahatama <ricky.prahatama@gmail.com>
2015-06-03 16:36:39 +0700

    Update Mobile Layout

---
+++ Lunggo.CustomerWeb/Assets/js/mobile-app.js
@@ -0,0 +1,32 @@
+﻿// on document ready
+$(document).ready(function() {
+    siteHeaderFunctions();
+});
+
+// site header functions
+function siteHeaderFunctions() {
+    siteMenuFunctions();
+    userMenuFunctions();
+
+    // site menu functions
+    function siteMenuFunctions() {
+        $('.site-menu').click(function () {
+            $('.user-menu').removeClass('active');
+            $('.user-menu-content').hide();
+            $(this).toggleClass('active');
+            $('.site-menu-content').toggle();
+        });
+    }
+
+    // user menu functions
+    function userMenuFunctions() {
+        $('.user-menu').click(function () {
+            $('.site-menu').removeClass('active');
+            $('.site-menu-content').hide();
+            $(this).toggleClass('active');
+            $('.user-menu-content').toggle();
+        });
+    }
+
+}
+
---
+++ Lunggo.CustomerWeb/Controllers/MobileTemplateController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+
+namespace Lunggo.CustomerWeb.Controllers
+{
+    public class MobileTemplateController : Controller
+    {
+        // GET: Template
+        public ActionResult HomePage()
+        {
+            return View();
+        }
+
+    }
+}
