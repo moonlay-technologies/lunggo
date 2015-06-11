89e4bb565342e51a10738c36d954469ddec4db62
Indera Aji Waskitho <indera.aji@travelmadezy.com>
2015-06-11 11:39:39 +0700

    EvoPDF NuGet

---
+++ Lunggo.Framework/HtmlTemplate/HtmlTemplateClient.cs
@@ -0,0 +1,8 @@
+﻿namespace Lunggo.Framework.HtmlTemplate
+{
+    internal abstract class HtmlTemplateClient
+    {
+        internal abstract void Init();
+        internal abstract string GenerateTemplate<T>(T objectParam, HtmlTemplateType type);
+    }
+}
