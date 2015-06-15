820b2bedda89369258352a81a11e004a8114d7fa
Rama Adhitia <rama.adhitia@travelmadezy.com>
2015-06-15 18:47:31 +0700

    Add BlajarWebJobProject

---
+++ BlajarWebjob/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+
+namespace BlajarWebjob
+{
+    // To learn more about Microsoft Azure WebJobs SDK, please see http://go.microsoft.com/fwlink/?LinkID=320976
+    class Program
+    {
+        // Please set the following connection strings in app.config for this WebJob to run:
+        // AzureWebJobsDashboard and AzureWebJobsStorage
+        static void Main()
+        {
+            var configuration = new JobHostConfiguration
+            {
+                DashboardConnectionString = "DefaultEndpointsProtocol=https;AccountName=lunggostoragedev;AccountKey=lFA73VFWzxbPnk3hCJ17KVv4TSrfwA4zjfzM8PGno95xygdUomFq+AaNsKBGLn1wfaLmQCayb7Yt5x7z8/6fOg==",
+                StorageConnectionString = "DefaultEndpointsProtocol=https;AccountName=lunggostoragedev;AccountKey=lFA73VFWzxbPnk3hCJ17KVv4TSrfwA4zjfzM8PGno95xygdUomFq+AaNsKBGLn1wfaLmQCayb7Yt5x7z8/6fOg==",
+            };
+            var host = new JobHost(configuration);
+            // The following code ensures that the WebJob will be running continuously
+            host.RunAndBlock();
+        }
+    }
+}
