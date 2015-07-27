fc592b9e6906941fd6f8f0db4d05881fde617a5e
Indera Aji Waskitho <indera.aji@travelmadezy.com>
2015-07-27 15:52:15 +0700

    buang lunggo.cw

---
+++ Lunggo.BackendWeb/Controllers/FlightController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lunggo.BackendWeb.Controllers
+{
+    public class FlightController : Controller
+    {
+        public ActionResult Search()
+        {
+            return View();
+        }
+    }
+}
