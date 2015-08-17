f18888f76d2a053a17fd5e589cf68b5c2e51e438
Rama Adhitia <rama.adhitia@travelmadezy.com>
2015-08-17 19:40:43 +0700

    translate lagi dan lagi add language switcher

--- Lunggo.Framework/HtmlHelperExtension/HtmlHelperExtension.cs
+++ Lunggo.Framework/HtmlHelperExtension/HtmlHelperExtension.cs
@@ -6,7 +6,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Lunggo.Framework.Constant;
 using Lunggo.Framework.Context;
+using Lunggo.Framework.Extension;
 using Lunggo.Framework.I18nMessage;
 
 namespace Lunggo.Framework.HtmlHelperExtension
@@ -61,6 +63,61 @@ namespace Lunggo.Framework.HtmlHelperExtension
             var messageManager = MessageManager.GetInstance();
             var activeLangCode = OnlineContext.GetActiveLanguageCode();
             return MvcHtmlString.Create(messageManager.GetMessageValue(code,LangToIsoCodeMapper.GetIsoCode(activeLangCode)));
+        }
+
+        public static MvcHtmlString IdLangSwitcherUrl(this HtmlHelper helper)
+        {
+            return CreateLangSwitcherUrl(SystemConstant.IndonesianLanguageCode);
+        }
+
+        public static MvcHtmlString EnLangSwitcherUrl(this HtmlHelper helper)
+        {
+            return CreateLangSwitcherUrl(SystemConstant.EnglishLanguageCode);
+        }
+
+        private static MvcHtmlString CreateLangSwitcherUrl(String langCode)
+        {
+            var absolutePath = HttpContext.Current.Request.Url.AbsolutePath;
+            var scheme = HttpContext.Current.Request.Url.Scheme;
+            var host = HttpContext.Current.Request.Url.Host;
+            var port = HttpContext.Current.Request.Url.Port;
+            var query = HttpContext.Current.Request.Url.Query;
+            String newAbsolutePath = null;
+            
+            if (absolutePath == "/")
+            {
+                var builder = new StringBuilder();
+                builder.Append("/");
+                builder.Append(langCode);
+                newAbsolutePath = builder.ToString();
+            }
+            else
+            {
+                var builder = new StringBuilder();
+                builder.Append("/");
+                builder.Append(langCode);
+                if (absolutePath.Length > 3)
+                {
+                    builder.Append(absolutePath.Substring(3,absolutePath.Length - 3 - 1));    
+                }
+                newAbsolutePath = builder.ToString();
+            }
+
+            var urlBuilder = new StringBuilder();
+            urlBuilder.Append(scheme);
+            urlBuilder.Append(System.Uri.SchemeDelimiter);
+            urlBuilder.Append(host);
+            if (port != 80 && port > 0)
+            {
+                urlBuilder.Append(":");
+                urlBuilder.Append(port);
+            }
+            urlBuilder.Append(newAbsolutePath);
+            urlBuilder.Append("/");
+            urlBuilder.Append(query);
+            var returnUrl = urlBuilder.ToString();
+
+            return MvcHtmlString.Create(returnUrl);
         }
 
     }
