280355637e91628627d74fcc73e0ef57a45ff95a
Yoga Dwi Sukma <yoga.dwi@travelmadezy.com>
2015-09-07 10:29:18 +0700

    exchange rate & confirmation report backendweb for admin

---
+++ Lunggo.BackendWeb/Models/CheckReservationControllerModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lunggo.ApCommon.Flight.Model;
+using Lunggo.ApCommon.Payment.Constant;
+using Lunggo.ApCommon.Payment.Model;
+
+namespace Lunggo.BackendWeb.Models
+{
+    public class CheckReservationControllerModel
+    {
+        public string NoRsv { get; set; }
+        public PaymentInfo PaymentInfo { get; set; }
+        public PaymentStatus Status { get; set; }
+    }
+}
