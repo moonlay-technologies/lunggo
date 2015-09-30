a692a75ec8a3e7ff4b94441cede158d76ea32144
Indera Aji Waskitho <indera.aji@travelmadezy.com>
2015-09-30 19:39:14 +0700

    AirAsiaWrapper method prototypes

---
+++ Lunggo.ApCommon/Flight/Wrapper/AirAsia/AirAsiaClientHandler.cs
@@ -0,0 +1,35 @@
+﻿using Lunggo.Framework.Config;
+
+namespace Lunggo.ApCommon.Flight.Wrapper.AirAsia
+{
+    internal partial class AirAsiaWrapper
+    {
+        private class AirAsiaClientHandler
+        {
+            private static readonly AirAsiaClientHandler ClientInstance = new AirAsiaClientHandler();
+            private bool _isInitialized;
+            private static string _userName;
+            private static string _password;
+
+            private AirAsiaClientHandler()
+            {
+            
+            }
+
+            internal static AirAsiaClientHandler GetClientInstance()
+            {
+                return ClientInstance;
+            }
+
+            internal void Init()
+            {
+                if (!_isInitialized)
+                {
+                    //_userName = ConfigManager.GetInstance().GetConfigValue("AirAsia", "agentUserName");
+                    //_password = ConfigManager.GetInstance().GetConfigValue("AirAsia", "agentPassword");
+                    _isInitialized = true;
+                }
+            }
+        }
+    }
+}
