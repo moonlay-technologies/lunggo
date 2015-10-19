e0c1839ba1b6dd8fbefe2167b80c5c4cabdb9943
Ricky Prahatama <ricky.prahatama@gmail.com>
2015-10-19 17:02:20 +0700

    Update

--- Lunggo.CustomerWeb/Assets/travorama20/js/checkoutController.js
+++ Lunggo.CustomerWeb/Assets/travorama20/js/checkoutController.js
@@ -1,9 +1,36 @@
 ﻿// travorama angular app - checkout controller
+app.controller('checkoutController', [
+    '$http', '$scope', function($http, $scope) {
 
-app.controller('returnFlightController', [
-    '$http', '$scope', function ($http, $scope) {
+        //********************
+        // variables
+        $scope.currentPage = 1;
+        $scope.loginShown = false;
+        $scope.stepClass = '';
+
+        //********************
+        // general functions
+        // change page
+        $scope.changePage = function (page) {
+            // change current page variable
+            $scope.currentPage = page;
+            // change step class
+            $scope.stepClass = 'active-' + page;
+        }
+        // change page after login
+        $scope.changePage(currentPage);
+
+        // toggle Travorama Login
+        $scope.toggleLogin = function() {
+            if ($scope.loginShown == false) {
+                $scope.loginShown = true;
+            } else {
+                $scope.loginShown = false;
+            }
+        }
 
         
+
 
     }
 ]);// checkout controller
