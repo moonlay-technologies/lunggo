fa2ff18cd8d4474573383de7113639f4d4b08f7e
Ricky Prahatama <ricky.prahatama@gmail.com>
2015-10-19 16:09:32 +0700

    Preparation for Checkout Controller

---
+++ Lunggo.CustomerWeb/Assets/travorama20/js/checkoutController.js
@@ -0,0 +1,9 @@
+﻿// travorama angular app - checkout controller
+
+app.controller('returnFlightController', [
+    '$http', '$scope', function ($http, $scope) {
+
+        
+
+    }
+]);// checkout controller
