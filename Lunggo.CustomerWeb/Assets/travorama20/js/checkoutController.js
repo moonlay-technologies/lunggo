cc59a343d451b16a6dda1a49d88799521bb8005c
Ricky Prahatama <ricky.prahatama@gmail.com>
2015-10-20 15:02:31 +0700

    Update Form

--- Lunggo.CustomerWeb/Assets/travorama20/js/checkoutController.js
+++ Lunggo.CustomerWeb/Assets/travorama20/js/checkoutController.js
@@ -28,21 +28,6 @@ app.controller('checkoutController', [
         $scope.idRequired = idRequired;
         $scope.nationalityRequired = nationalityRequired;
 
-        $scope.months = [
-            { name: 'January', value: 1 },
-            { name: 'February', value: 2 },
-            { name: 'March', value: 3 },
-            { name: 'April', value: 4 },
-            { name: 'May', value: 5 },
-            { name: 'June', value: 6 },
-            { name: 'July', value: 7 },
-            { name: 'August', value: 8 },
-            { name: 'September', value: 9 },
-            { name: 'October', value: 10 },
-            { name: 'November', value: 11 },
-            { name: 'December', value: 12 }
-        ];
-
         $scope.countries = ["Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Anguilla", "Antigua &amp; Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia &amp; Herzegovina", "Botswana", "Brazil", "British Virgin Islands", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Cape Verde", "Cayman Islands", "Chad", "Chile", "China", "Colombia", "Congo", "Cook Islands", "Costa Rica", "Cote D Ivoire", "Croatia", "Cruise Ship", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Estonia", "Ethiopia", "Falkland Islands", "Faroe Islands", "Fiji", "Finland", "France", "French Polynesia", "French West Indies", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guam", "Guatemala", "Guernsey", "Guinea", "Guinea Bissau", "Guyana", "Haiti", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Isle of Man", "Israel", "Italy", "Jamaica", "Japan", "Jersey", "Jordan", "Kazakhstan", "Kenya", "Kuwait", "Kyrgyz Republic", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Mauritania", "Mauritius", "Mexico", "Moldova", "Monaco", "Mongolia", "Montenegro", "Montserrat", "Morocco", "Mozambique", "Namibia", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Norway", "Oman", "Pakistan", "Palestine", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russia", "Rwanda", "Saint Pierre &amp; Miquelon", "Samoa", "San Marino", "Satellite", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "South Africa", "South Korea", "Spain", "Sri Lanka", "St Kitts &amp; Nevis", "St Lucia", "St Vincent", "St. Lucia", "Sudan", "Suriname", "Swaziland", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Timor L'Este", "Togo", "Tonga", "Trinidad &amp; Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks &amp; Caicos", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "Uruguay", "Uzbekistan", "Venezuela", "Vietnam", "Virgin Islands (US)", "Yemen", "Zambia", "Zimbabwe"];
 
         //********************
@@ -60,19 +45,19 @@ app.controller('checkoutController', [
         $scope.generatePassenger = function () {
             if (adultPassenger > 0) {
                 for (var i = 0; i < adultPassenger; i++) {
-                    var x = { type: adultTypeName };
+                    var x = { typeName: adultTypeName, type: 'adult' };
                     $scope.passengers.push( x );
                 }
             }
             if (childPassenger > 0) {
                 for (var i = 0; i < childPassenger; i++) {
-                    var x = { type: childTypeName };
+                    var x = { typeName: childTypeName, type: 'child' };
                     $scope.passengers.push(x);
                 }
             }
             if (infantPassenger > 0) {
                 for (var i = 0; i < infantPassenger; i++) {
-                    var x = { type: infantTypeName };
+                    var x = { typeName: infantTypeName, type: 'infant' };
                     $scope.passengers.push(x);
                 }
             }
@@ -116,11 +101,11 @@ app.controller('checkoutController', [
             if (formSelect == "buyerForm") {
                 console.log($scope.buyerForm);
             } else if (formSelect == "passenger") {
-                console.log('Adult Passengers :');
-                console.log($scope.adultPassenger);
                 // console.log($scope.adult1Form);
             } else {
                 console.log($scope);
+                console.log('Passenger form :');
+                console.log($scope.passengerForm_);
             }
         }
 
