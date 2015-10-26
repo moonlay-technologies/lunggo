a357a4e8d5b7c44ba00a833f74a3902bd995fb8d
bayualvian <bayualvian@hotmail.com>
2015-10-26 12:07:53 +0800

    UW620

--- Lunggo.CustomerWeb/WebSrc/UW600/UW620/UW620OrderHistoryController.cs
+++ Lunggo.CustomerWeb/WebSrc/UW600/UW620/UW620OrderHistoryController.cs
@@ -9,6 +9,8 @@ using Lunggo.ApCommon.Identity.User;
 using Lunggo.CustomerWeb.WebSrc.UW600.UW620.Logic;
 using Lunggo.CustomerWeb.WebSrc.UW600.UW620.Object;
 using Lunggo.CustomerWeb.WebSrc.UW600.UW620.Model;
+using System.IO;
+using Lunggo.Framework.HtmlTemplate;
 
 namespace Lunggo.CustomerWeb.WebSrc.UW600.UW620
 {
@@ -29,6 +31,34 @@ namespace Lunggo.CustomerWeb.WebSrc.UW600.UW620
             var flight = FlightService.GetInstance();
             var reservation = flight.GetReservationForDisplay(rsvNo);
             return View(reservation);
+        }
+
+        public FileStreamResult OrderFlightHistoryDetailPrint(string rsvNo)
+        {
+            Stream fileStream = GeneratePDF(rsvNo);
+
+            HttpContext.Response.AddHeader("content-disposition",
+            "attachment; filename=" + rsvNo + "pdf");
+
+            return new FileStreamResult(fileStream, "application/pdf");
+        }
+        private Stream GeneratePDF(string rsvNo)
+        {
+            var flightService = FlightService.GetInstance();
+            var templateService = HtmlTemplateService.GetInstance();
+            var converter = new SelectPdf.HtmlToPdf();
+            var reservation = flightService.GetDetails(rsvNo);
+            var eticketTemplate = templateService.GenerateTemplate(reservation, "FlightEticket");
+
+            var eticketFile = converter.ConvertHtmlString(eticketTemplate).Save();
+
+            MemoryStream ms = new MemoryStream();
+
+            byte[] byteInfo = eticketFile;
+            ms.Write(byteInfo, 0, byteInfo.Length);
+            ms.Position = 0;
+
+            return ms;
         }
     }
 }
