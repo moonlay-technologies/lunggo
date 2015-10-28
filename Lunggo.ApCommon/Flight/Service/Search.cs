8350a63db792e616c025b9b356cafc1f324af9f0
Indera Aji Waskitho <indera.aji@travelmadezy.com>
2015-10-28 14:58:05 +0700

    ketinggalan dikit

--- Lunggo.ApCommon/Flight/Service/Search.cs
+++ Lunggo.ApCommon/Flight/Service/Search.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Security.Policy;
 using System.Threading;
 using System.Threading.Tasks;
+using CsQuery.ExtensionMethods.Internal;
 using Lunggo.ApCommon.Constant;
 using Lunggo.ApCommon.Flight.Model;
 using Lunggo.ApCommon.Flight.Model.Logic;
@@ -29,23 +30,36 @@ namespace Lunggo.ApCommon.Flight.Service
             var output = new SearchFlightOutput();
             var searchId = EncodeConditions(input.Conditions);
 
-            var searchedItins = GetSearchedSupplierItineraries(searchId, input.RequestedSupplierIds);
-            var searchedSuppliers = searchedItins.Keys.ToList();
-            var missingSuppliers = input.RequestedSupplierIds.Except(searchedSuppliers).ToList();
-            foreach (var missingSupplier in missingSuppliers)
+            if (input.RequestedSupplierIds.IsNullOrEmpty())
             {
-                var queue = QueueService.GetInstance().GetQueueByReference("FlightCrawler" + missingSupplier);
-                queue.AddMessage(new CloudQueueMessage(searchId));
+                output.IsSuccess = true;
+                output.TotalSupplier = Suppliers.Count;
+                output.Itineraries = new List<FlightItineraryForDisplay>();
+                output.SearchId = searchId;
             }
+            else
+            {
 
-            var itinsForDisplay = searchedItins.SelectMany(dict => dict.Value).Select(ConvertToItineraryForDisplay).ToList();
-            itinsForDisplay.ForEach(itin => itin.SearchId = output.SearchId);
+                var searchedItins = GetSearchedSupplierItineraries(searchId, input.RequestedSupplierIds);
+                var searchedSuppliers = searchedItins.Keys.ToList();
+                var missingSuppliers = input.RequestedSupplierIds.Except(searchedSuppliers).ToList();
+                foreach (var missingSupplier in missingSuppliers)
+                {
+                    var queue = QueueService.GetInstance().GetQueueByReference("FlightCrawler" + missingSupplier);
+                    queue.AddMessage(new CloudQueueMessage(searchId));
+                }
 
-            output.IsSuccess = true;
-            output.SearchId = searchId;
-            output.ExpiryTime = GetSearchedItinerariesExpiry(searchId);
-            output.Itineraries = itinsForDisplay;
-            output.SearchedSuppliers = searchedSuppliers;
+                var itinsForDisplay =
+                    searchedItins.SelectMany(dict => dict.Value).Select(ConvertToItineraryForDisplay).ToList();
+                itinsForDisplay.ForEach(itin => itin.SearchId = output.SearchId);
+
+                output.IsSuccess = true;
+                output.SearchId = searchId;
+                output.ExpiryTime = GetSearchedItinerariesExpiry(searchId);
+                output.Itineraries = itinsForDisplay;
+                output.SearchedSuppliers = searchedSuppliers;
+                output.TotalSupplier = Suppliers.Count;
+            }
 
             return output;
         }
