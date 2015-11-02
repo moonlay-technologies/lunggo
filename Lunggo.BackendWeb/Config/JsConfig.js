698153c7e8f3f242bc28177445de78d4d981f943
Ricky Prahatama <ricky.prahatama@gmail.com>
2015-11-02 16:25:08 +0700

    Update Flight Pages

--- Lunggo.BackendWeb/Config/JsConfig.js
+++ Lunggo.BackendWeb/Config/JsConfig.js
@@ -5,49 +5,49 @@ var SystemConfig = {
 };
 
 var SearchHotelConfig = {
-    Url: 'http://travorama-qa-api.azurewebsites.net/api/v1/hotels',
+    Url: 'http://localhost:1147/api/v1/hotels',
     ResultCount: 24
 };
 
 var SearchRoomConfig = {
-    Url: 'http://travorama-qa-api.azurewebsites.net/api/v1/rooms'
+    Url: 'http://localhost:1147/api/v1/rooms'
 };
 
 var FlightSearchConfig = {
-    Url: 'http://travorama-qa-api.azurewebsites.net/api/v1/flights'
+    Url: 'http://localhost:1147/api/v1/flights'
 };
 
 var RevalidateConfig = {
-    Url: 'http://travorama-qa-api.azurewebsites.net/api/v1/flights/revalidate',
+    Url: 'http://localhost:1147/api/v1/flights/revalidate',
     working: false
 };
 
 var FlightBookConfig = {
-    Url: 'http://travorama-qa-api.azurewebsites.net/api/v1/flights/book',
+    Url: 'http://localhost:1147/api/v1/flights/book',
     working: false
 };
 
 var GetRulesConfig = {
-    Url: 'http://travorama-qa-api.azurewebsites.net/api/v1/flights/rules',
+    Url: 'http://localhost:1147/api/v1/flights/rules',
     working: false
 };
 
 var HotelAutocompleteConfig = {
-    Url: 'http://travorama-qa-api.azurewebsites.net/api/v1/autocomplete/hotellocation/'
+    Url: 'http://localhost:1147/api/v1/autocomplete/hotellocation/'
 };
 
 var FlightAutocompleteConfig = {
-    Url: 'http://travorama-qa-api.azurewebsites.net/api/v1/autocomplete/airport/'
+    Url: 'http://localhost:1147/api/v1/autocomplete/airport/'
 };
 
 var AirlineAutocompleteConfig = {
-    Url: 'http://travorama-qa-api.azurewebsites.net/api/v1/autocomplete/airline/'
+    Url: 'http://localhost:1147/api/v1/autocomplete/airline/'
 };
 
 var CheckVoucherConfig = {
-    Url: 'http://travorama-qa-api.azurewebsites.net/api/v1/voucher/check'
+    Url: 'http://localhost:1147/api/v1/voucher/check'
 };
 
 var SubscribeConfig = {
-    Url: 'http://travorama-qa-api.azurewebsites.net/api/v1/newsletter/subscribe'
+    Url: 'http://localhost:1147/api/v1/newsletter/subscribe'
 };
