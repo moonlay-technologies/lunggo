9e90ef24af1b8a1c114eeb1840b55243ccccb583
Yoga Dwi Sukma <yoga.dwi@travelmadezy.com>
2015-11-03 16:45:04 +0700

    ambil perbaikan indera

--- Lunggo.CustomerWeb/Config/JsConfig.js
+++ Lunggo.CustomerWeb/Config/JsConfig.js
@@ -51,3 +51,19 @@ var CheckVoucherConfig = {
 var SubscribeConfig = {
     Url: 'http://localhost:1147/api/v1/newsletter/subscribe'
 };
+
+var ResetPasswordConfig = {
+    Url: 'http://localhost:23321/id/ApiAccount/ResetPassword'
+};
+
+var ForgotPasswordConfig = {
+    Url: 'http://localhost:23321/id/ApiAccount/ForgotPassword'
+};
+
+var ChangePasswordConfig = {
+    Url: 'http://localhost:23321/id/ApiAccount/ChangePassword'
+};
+
+var ChangeProfileConfig = {
+    Url: 'http://localhost:23321/id/ApiAccount/ChangeProfile'
+};
