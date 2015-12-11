7ec5ecc43796cea52e7b5ee0083065567a1272cf
Suheri Marpaung <suheri@travelmadezy.com>
2015-12-11 19:16:24 +0700

    fixing hardcode email and get calendar

---
+++ Lunggo.Repo/TableRecord/CalendarRecipientTableRecord.cs
@@ -0,0 +1,129 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Lunggo.Framework.Database;
+
+namespace Lunggo.Repository.TableRecord
+{
+    public class CalendarRecipientTableRecord : Lunggo.Framework.Database.TableRecord
+    {
+		private static List<ColumnMetadata> _recordMetadata;
+        private static List<ColumnMetadata> _primaryKeys;
+        private static String _tableName;
+
+		public int? Id
+		{
+		    get { return _Id; }
+		    set
+		    {
+		        _Id = value;
+		        IncrementLog("Id");
+		    }
+		}
+		public String Email
+		{
+		    get { return _Email; }
+		    set
+		    {
+		        _Email = value;
+		        IncrementLog("Email");
+		    }
+		}
+		public String Name
+		{
+		    get { return _Name; }
+		    set
+		    {
+		        _Name = value;
+		        IncrementLog("Name");
+		    }
+		}
+		public String PhoneNumber
+		{
+		    get { return _PhoneNumber; }
+		    set
+		    {
+		        _PhoneNumber = value;
+		        IncrementLog("PhoneNumber");
+		    }
+		}
+		public String Address
+		{
+		    get { return _Address; }
+		    set
+		    {
+		        _Address = value;
+		        IncrementLog("Address");
+		    }
+		}
+
+		
+		private int? _Id;
+		private String _Email;
+		private String _Name;
+		private String _PhoneNumber;
+		private String _Address;
+
+
+		public static CalendarRecipientTableRecord CreateNewInstance()
+        {
+            var record = new CalendarRecipientTableRecord();
+            var iRecord = record.AsInterface();
+            iRecord.ManuallyCreated = true;
+            return record;
+        }
+
+		public CalendarRecipientTableRecord()
+        {
+            ;
+        }
+
+        static CalendarRecipientTableRecord()
+        {
+            InitTableName();
+            InitRecordMetadata();
+            InitPrimaryKeysMetadata();
+        }
+
+        private static void InitTableName()
+        {
+            _tableName = "CalendarRecipient";
+        }
+
+        private static void InitRecordMetadata()
+        {
+            _recordMetadata = new List<ColumnMetadata>
+            {
+				new ColumnMetadata("Id", true),
+				new ColumnMetadata("Email", false),
+				new ColumnMetadata("Name", false),
+				new ColumnMetadata("PhoneNumber", false),
+				new ColumnMetadata("Address", false),
+
+            };
+        }
+
+        private static void InitPrimaryKeysMetadata()
+        {
+            _primaryKeys = _recordMetadata.Where(p => p.IsPrimaryKey).ToList();
+        }
+
+		public override List<ColumnMetadata> GetMetadata()
+        {
+            return _recordMetadata;
+        }
+
+        public override string GetTableName()
+        {
+            return _tableName;
+        }
+
+        public override List<ColumnMetadata> GetPrimaryKeys()
+        {
+            return _primaryKeys;
+        }
+
+
+
+    }
+}
