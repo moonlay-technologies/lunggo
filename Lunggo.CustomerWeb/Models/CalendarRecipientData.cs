6f31a9bc95d0a9f7b9dff55d4928ecd89ad26faa
Suheri Marpaung <suheri@travelmadezy.com>
2015-12-14 14:10:37 +0700

    Perbaharui model getCalendar

--- Lunggo.CustomerWeb/Models/CalendarRecipientData.cs
+++ Lunggo.CustomerWeb/Models/CalendarRecipientData.cs
@@ -9,18 +9,23 @@ namespace Lunggo.CustomerWeb.Models
         [Display(Name = "Email")]
         public string Email { get; set; }
 
+        [Required]
         [Display(Name = "Name")]
         public string Name { get; set; }
 
+        [Required]
         [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; }
 
+        [Required]
         [Display(Name = "City")]
         public string City { get; set; }
 
+        [Required]
         [Display(Name = "Postal Code")]
         public string PostalCode { get; set; }
 
+        [Required]
         [Display(Name = "Address")]
         [DataType(DataType.MultilineText)] 
         public string Address { get; set; }
