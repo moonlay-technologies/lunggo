c4e64d70967ea38d5c65f1475487c04636c3a6a9
Ricky Prahatama <ricky.prahatama@gmail.com>
2015-12-17 09:34:57 +0700

    Added Burger Menu function for Mobile View

--- Lunggo.CustomerWeb/Assets/mobile/js/application.js
+++ Lunggo.CustomerWeb/Assets/mobile/js/application.js
@@ -1,6 +1,46 @@
 ﻿// check if angular exist
 if (typeof (angular) == 'object') {
     var app = angular.module('Travorama', ['ngRoute']);
+
+    // root scope
+    app.run(function($rootScope) {
+
+        // general page config and function
+        $rootScope.PageConfig = {
+            
+            // **********
+            // variables
+            Loaded: true,
+            Busy: false,
+
+            // **********
+            // functions
+
+            // body no scroll
+            BodyNoScroll: false,
+            SetNoScroll: function (state) {
+
+            },
+
+            // toggle burger menu
+            BurgerShown: false,
+            ToggleBurger : function() {
+
+                if ($rootScope.PageConfig.BurgerShown == false) {
+                    $rootScope.PageConfig.BurgerShown = true;
+                    $rootScope.PageConfig.BodyNoScroll = true;
+                } else {
+                    $rootScope.PageConfig.BurgerShown = false;
+                    $rootScope.PageConfig.BodyNoScroll = false;
+                }
+
+            }
+
+        };
+
+
+    });
+
 }
 
 // --------------------
