2c73ec343435f46fd9b5e8cd3950f3f61b4fb64b
Ricky Prahatama <ricky.prahatama@gmail.com>
2015-12-22 11:30:17 +0700

    Mobile Checkout Page. Added Voucher Validation.

--- Lunggo.CustomerWeb/Assets/mobile/js/CheckoutController.js
+++ Lunggo.CustomerWeb/Assets/mobile/js/CheckoutController.js
@@ -1,15 +1,36 @@
 ﻿app.controller('CheckoutController', ['$http', '$scope', '$rootScope', '$interval', '$location', function($http, $scope, $rootScope, $interval, $location) {
     
+    // general variables
+    $scope.CheckoutConfig = {
+        Token: CheckoutDetail.Token,
+        ExpiryDate: CheckoutDetail.ExpiryDate,
+        // trips
+        Trips: CheckoutDetail.Trips,
+        // price
+        Price: CheckoutDetail.Price,
+        FinalPrice: CheckoutDetail.Price,
+        // passenger
+        Passenger: CheckoutDetail.Passenger,
+        // identity requirement
+        PassportRequired: CheckoutDetail.PassportRequired,
+        IdRequired: CheckoutDetail.IdRequired,
+        NationalityRequired: CheckoutDetail.NationalityRequired,
+        // buyer info
+        BuyerInfo: CheckoutDetail.BuyerInfo
+    };
+
+
     // return URL
     $scope.returnUrl = document.referrer == (window.location.origin + window.location.pathname + window.location.search) ? '/' : document.referrer;
 
+    // *****
+    // paging function
     // set hash to page 1
     angular.element(document).ready(function () {
         //$scope.PageConfig.ActivePage = 1;
         $location.hash('page-1');
         console.log('READY');
     });
-
     angular.element(window).on('hashchange', function () {
         if ($location.hash() == '') {
             $scope.PageConfig.ActivePage = 1;
@@ -30,9 +51,6 @@
             }
         }
     });
-
-    // get hash to change page for back page
-
     // change page
     $scope.PageConfig.ActivePageChanged = false;
     $scope.PageConfig.ActivePage = 1;
@@ -40,6 +58,64 @@
         $scope.PageConfig.ActivePage = page;
         $location.hash("page-"+page);
     }
+    // paging function end
+    // *****
+
+    // *****
+    // voucher function
+    $scope.CheckoutConfig.Voucher = {
+        Code: '',
+        Validated: false,
+        Validating: false,
+        Valid: false,
+        Amount: 0,
+        Status: '',
+        // validate voucher
+        Check: function () {
+            console.log('Validating Voucher');
+            $scope.CheckoutConfig.Voucher.Validating = true;
+            $http({
+                method: 'GET',
+                url: CheckVoucherConfig.Url,
+                params: {
+                    token: $scope.CheckoutConfig.Token,
+                    code: $scope.CheckoutConfig.Voucher.Code,
+                    email: $scope.CheckoutConfig.BuyerInfo.Email,
+                    price: $scope.CheckoutConfig.Price
+                }
+            }).then(function (returnData) {
+                console.log('Voucher code validated :');
+                console.log(returnData);
+                $scope.CheckoutConfig.Voucher.Validating = false;
+                $scope.CheckoutConfig.Voucher.Validated = true;
+                $scope.CheckoutConfig.Voucher.Status = returnData.data.ValidationStatus;
+                if (returnData.data.Discount > 0) {
+                    $scope.CheckoutConfig.Voucher.Valid = true;
+                    $scope.CheckoutConfig.Voucher.Amount = returnData.data.Discount;
+                } else {
+                    $scope.CheckoutConfig.Voucher.Valid = false;
+                    $scope.CheckoutConfig.Voucher.Status = returnData.data.ValidationStatus;
+                }
+            }, function (returnData) {
+                $scope.CheckoutConfig.Voucher.Validated = true;
+                $scope.CheckoutConfig.Voucher.Validating = false;
+                $scope.CheckoutConfig.Voucher.Valid = false;
+                console.log('Error validating voucher. Reason : ');
+                console.log(returnData);
+            });
+        },
+        // reset voucher
+        Reset: function() {
+            $scope.CheckoutConfig.Voucher.Validated = false;
+            $scope.CheckoutConfig.Voucher.Code = '';
+            $scope.CheckoutConfig.Voucher.Valid = false;
+            $scope.CheckoutConfig.Voucher.Status = '';
+            $scope.CheckoutConfig.Voucher.Amount = -1;
+        }
+    };
+
+    // voucher function end
+    // *****
 
 
 }]);
