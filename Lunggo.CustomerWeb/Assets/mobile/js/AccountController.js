be29b44863ac25495194ff9c39c73bc42645b87d
Ricky Prahatama <ricky.prahatama@gmail.com>
2015-12-28 15:51:49 +0700

    Mobile. Added Login Page.

---
+++ Lunggo.CustomerWeb/Assets/mobile/js/AccountController.js
@@ -0,0 +1,74 @@
+﻿// Login Controller
+app.controller('LoginController', ['$http', '$scope', '$rootScope', function($http, $scope, $rootScope) {
+
+    $scope.PageConfig = $rootScope.PageConfig;
+
+    $scope.User = {
+        Email: '',
+        Password: '',
+        Message: LoginMessage,
+        Sending: false,
+        Sent: false,
+        Send: function() {
+            $scope.User.Sending = true;
+            $('.login-form').submit();
+        }
+    };
+
+    console.log($scope.User.Message);
+
+}]);// Login Controller
+
+// Forgot Password Controller
+app.controller('ForgotpasswordController', ['$http', '$scope', '$rootScope', function ($http, $scope, $rootScope) {
+
+    $scope.PageConfig = $rootScope.PageConfig;
+
+    $scope.EmailForm = {
+        Email: "",
+        Sending: false,
+        Sent: false,
+        ReturnData : {
+            Found: false,
+            EmailConfirmed: false
+        },
+        SendForm: function () {
+            $scope.EmailForm.Sending = true;
+            // send form
+            // submit form to URL
+            $http({
+                url: ForgotPasswordConfig.Url,
+                method: 'POST',
+                data: {
+                    email: $scope.EmailForm.Email
+                }
+            }).then(function (returnData) {
+                $scope.EmailForm.Sending = false;
+                $scope.EmailForm.Sent = true;
+                console.log(returnData);
+
+                switch (returnData.data.Status) {
+                    case "Success":
+                        $scope.EmailForm.ReturnData.Found = true;
+                        $scope.EmailForm.ReturnData.EmailConfirmed = true;
+                        break;
+                    case "NotRegistered":
+                        $scope.EmailForm.ReturnData.Found = false;
+                        break;
+                    case "AlreadyRegisteredButUnconfirmed":
+                        $scope.EmailForm.ReturnData.Found = true;
+                        $scope.EmailForm.ReturnData.EmailConfirmed = false;
+                        break;
+                    case "InvalidInputData":
+                        $scope.EmailForm.ReturnData.Found = false;
+                        break;
+                }
+            }, function (returnData) {
+                console.log('Failed requesting reset password');
+                console.log(returnData);
+                $scope.EmailForm.Sending = false;
+            });
+        }
+    };
+
+}]);// ForgotPasswordController
