25b347127b376f958bd0938948d36a809acb3c5b
Indera Aji Waskitho <indera.aji@travelmadezy.com>
2016-02-02 13:54:49 +0700

    Imlek Promo API Dummy

---
+++ Lunggo.WebAPI/ApiSrc/v1/Promo/Imlek/ImlekController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Lunggo.Framework.Cors;
+
+namespace Lunggo.WebAPI.ApiSrc.v1.Promo.Imlek
+{
+    public class ImlekController : ApiController
+    {
+        [HttpPost]
+        [LunggoCorsPolicy]
+        [Route("v1/promo/imlek")]
+        public int PromoImlek(HttpRequestMessage httpRequest, [FromUri] string email)
+        {
+            return new Random().Next(-1,5);
+        }
+
+    }
+}
