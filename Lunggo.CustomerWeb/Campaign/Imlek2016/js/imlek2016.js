3d980472a783389b6d13e32fd33a9e5333c27f3f
Ricky Prahatama <ricky.prahatama@gmail.com>
2016-02-04 16:57:17 +0700

    Imlek. Update Static Page and View Page.

--- Lunggo.CustomerWeb/Campaign/Imlek2016/js/imlek2016.js
+++ Lunggo.CustomerWeb/Campaign/Imlek2016/js/imlek2016.js
@@ -3,6 +3,8 @@ var Imlek = {};
 
 // email
 Imlek.Email = '';
+Imlek.Url = '';
+Imlek.Eligible = true;
 
 // start or end the overlay
 Imlek.State = function(state) {
@@ -21,14 +23,25 @@ Imlek.State = function(state) {
 };
 
 // Add email
-Imlek.AddEmail = function(emailAddress) {
+Imlek.AddEmail = function (emailAddress) {
     Imlek.Email = emailAddress;
+    Imlek.State('init');
 }
 
 // submit email
-$('#imlek__email__submit').click(function() {
-    Imlek.AddEmail($('#imlek__email__address').val());
-    Imlek.State('init');
+$('#imlek__email__submit').click(function () {
+
+    var email = $('#imlek__email__address').val();
+    var re = /[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}/igm;
+
+    if ( email = '' || !re.test( email ) ) {
+        $(this).parent().addClass('has-error');
+        $(this).siblings('.help-block').show();
+    } else {
+        Imlek.Url = $(this).attr('data-url');
+        Imlek.AddEmail(email);
+    }
+
 });
 
 // card on hover
@@ -49,6 +62,46 @@ $('.imlek-wrapper .card').click(function(evt) {
     if ($(this).hasClass('active')) {
         return false;
     }
+
+    // API call
+    $.post((Imlek.Url + '/v1/promo/imlek' ), { email: Imlek.Email })
+    .done(function (data) {
+
+        // text target
+        var textTarget = $('.imlek-wrapper .card.active').children('.prize__wrapper').children('.prize__body');
+
+        var text = {};
+        text.noPrize = '<h3><b>Silahkan Coba Lagi</b></h3><p>Maaf, Anda belum beruntung. Silahkan coba lagi.</p>';
+        text.noEligible = '<h3><b>Sudah Habis</b></h3><p>Kesempatan Anda hari ini sudah habis. Silahkan coba lagi esok hari.</p>';
+
+        if (data.c < 0) {
+            textTarget.addClass('no-eligible');
+            textTarget.html(text.noEligible);
+            Imlek.Eligible = false;
+        } else if (data.c == 0) {
+            textTarget.addClass('no-prize');
+            textTarget.html(text.noPrize);
+        } else if (data.c > 0) {
+            textTarget.addClass('prize');
+            //data.v = "TRAVOR4M4";
+            switch (data.c) {
+                case 1:
+                    textTarget.addClass('prize-50');
+                    break;
+                case 2:
+                    textTarget.addClass('prize-100');
+                    break;
+                case 3:
+                    textTarget.addClass('prize-150');
+                    break;
+                case 4:
+                    textTarget.addClass('prize-200');
+                    break;
+            }
+            textTarget.html('<p class="text-info">Masukan kode voucher ini ketika melakukan pembayaran.</p><p class="voucher-code">'+data.v+'</p>');
+        }
+
+    });
 
     // add active class
     $(this).addClass('active');
@@ -81,6 +134,9 @@ $('.imlek-wrapper .card').click(function(evt) {
 $('.imlek-wrapper .card .prize__close').click(function(evt) {
     $(this).closest('.card').fadeOut(700, function() { $(this).remove(); });
     $('.imlek-wrapper .tree').children().show();
+    if (!Imlek.Eligible) {
+        Imlek.State('end');
+    }
 });
 
 // body no scroll
