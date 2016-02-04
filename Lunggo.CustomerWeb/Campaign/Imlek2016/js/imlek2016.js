7ba202827a64baf2c1759c71406503197de6e582
Ricky Prahatama <ricky.prahatama@gmail.com>
2016-02-04 11:00:41 +0700

    Added email input form

--- Lunggo.CustomerWeb/Campaign/Imlek2016/js/imlek2016.js
+++ Lunggo.CustomerWeb/Campaign/Imlek2016/js/imlek2016.js
@@ -1,26 +1,46 @@
 ﻿// variables
 var Imlek = {};
 
+// email
+Imlek.Email = '';
 
-// card animation
-Imlek.CardAnim = function(state) {
+// start or end the overlay
+Imlek.State = function(state) {
 
-    $('.imlek-wrapper .card').each(function () {
-
-        var max = 1500;
-        var min = 500;
-        var speed = (Math.random() * (max - min)) + min;
-
-        $(this).css({
-            transformOrigin: '50% 0'
-        }).transition({
-            rotate: '15deg'
-        }, speed, 'linear').transition({
-            rotate: '-15deg'
-        }, speed, 'linear');
-    });
+    if (state == 'start') {
+        $('.imlek-wrapper').fadeIn();
+        $('body').addClass('no-scroll');
+    } else if (state == 'end') {
+        $('.imlek-wrapper').fadeOut();
+        $('body').removeClass('no-scroll');
+    } else if (state == 'init') {
+        $('.imlek-wrapper .tree').fadeIn();
+        $('.imlek-wrapper .email').fadeOut();
+    }
 
 };
+
+// Add email
+Imlek.AddEmail = function(emailAddress) {
+    Imlek.Email = emailAddress;
+}
+
+// submit email
+$('#imlek__email__submit').click(function() {
+    Imlek.AddEmail($('#imlek__email__address').val());
+    Imlek.State('init');
+});
+
+// card on hover
+$('.imlek-wrapper .card').hover(function() {
+
+    // on mouse in
+
+}, function() {
+    
+    // on mouse out
+
+});
 
 // card on click
 $('.imlek-wrapper .card').click(function(evt) {
@@ -61,6 +81,16 @@ $('.imlek-wrapper .card').click(function(evt) {
 $('.imlek-wrapper .card .prize__close').click(function(evt) {
     $(this).closest('.card').fadeOut(700, function() { $(this).remove(); });
     $('.imlek-wrapper .tree').children().show();
+});
+
+// body no scroll
+$(document).ready(function() {
+    Imlek.State('start');
+});
+
+// imlek close
+$('.imlek-wrapper__close').click(function() {
+    Imlek.State('end');
 });
 
 
