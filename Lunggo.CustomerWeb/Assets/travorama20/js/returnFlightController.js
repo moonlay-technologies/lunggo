a565182910ccca9441dd2b5d4cb8617b57331ddd
Intan Dea Yutami <intan.yutami@travelmadezy.com>
2016-05-30 11:40:23 +0700

    filter udh bisa diclose tnp pake tombol X, return masih belum fully working

--- Lunggo.CustomerWeb/Assets/travorama20/js/returnFlightController.js
+++ Lunggo.CustomerWeb/Assets/travorama20/js/returnFlightController.js
@@ -847,8 +847,7 @@ app.controller('returnFlightController', [
 
         }
 
-
-
+        
     }
 ]);// flight controller
 
--- Lunggo.CustomerWeb/Assets/travorama20/js/travorama20.js
+++ Lunggo.CustomerWeb/Assets/travorama20/js/travorama20.js
@@ -255,19 +255,29 @@ function flightPageFunctions() {
     });
     // toggle filter
     $('.search-result-filter .filter-trigger span').click(function () {
-        $(this).parent().siblings().removeClass('active');
-        $(this).parent().addClass('active');
-        var targetFilter = $(this).attr('data-target');
-        $('.search-result-filter .filter-content>div').removeClass('active');
-        $('.search-result-filter .filter-content>div#'+targetFilter).addClass('active');
+        
+        if (ct % 2 == 0) {
+            $(this).parent().siblings().removeClass('active');
+            $(this).parent().addClass('active');
+            var targetFilter = $(this).attr('data-target');
+            $('.search-result-filter .filter-content>div').removeClass('active');
+            $('.search-result-filter .filter-content>div#' + targetFilter).addClass('active');
+            ct += 1;
+        } else {
+            $('.search-result-filter .close-filter').click();
+            ct += 1;
+        }
+        //$('.search-result-filter .filter-content>div#'+targetFilter).slideToggle("slow");
     });
     // close filter
     $('.search-result-filter .close-filter').click(function() {
         $(this).parent().removeClass('active');
         $(this).parent().parent().siblings('.filter-trigger').children().removeClass('active');
+        ct += 1;
     });
 }
 
+var ct = 0;
 var flightPageSearchFormParam = {
     type: 'return',
     origin: '',
