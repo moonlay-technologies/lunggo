27d75e1a827f6d80da76b6b49a46a1e7e3166d6d
Intan Dea Yutami <intan.yutami@travelmadezy.com>
2016-06-23 14:05:02 +0700

    commit dulu

--- Lunggo.ApCommon/Flight/Wrapper/Garuda/GarudaClientHandler.cs
+++ Lunggo.ApCommon/Flight/Wrapper/Garuda/GarudaClientHandler.cs
@@ -54,8 +54,8 @@ namespace Lunggo.ApCommon.Flight.Wrapper.Garuda
                 client.AddDefaultHeader("Upgrade-Insecure-Requests", "1");
                 client.UserAgent = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36";
                 client.CookieContainer = new CookieContainer();
-                client.Proxy = new WebProxy("185.77.167.128", 60000);
-                client.Proxy.Credentials = new NetworkCredential("travelmadezy", "9T8XCty9MT");
+                //client.Proxy = new WebProxy("185.77.167.128", 60000);
+                //client.Proxy.Credentials = new NetworkCredential("travelmadezy", "9T8XCty9MT");
                 //client.FollowRedirects = false;
                 return client;
             }
--- Lunggo.CustomerWeb/Assets/travorama20/js/checkoutController.js
+++ Lunggo.CustomerWeb/Assets/travorama20/js/checkoutController.js
@@ -162,7 +162,7 @@ app.controller('checkoutController', [
                 $scope.book.isPriceChanged = false;
                 $scope.book.checked = false;
                 $scope.contactData = '{' + ' "title":"' + $scope.buyerInfo.title + '",  "name" :"' + $scope.buyerInfo.name + '","countryCallCd":"' + $scope.buyerInfo.countryCode + '","phone":"' + $scope.buyerInfo.phone + '","email":"' + $scope.buyerInfo.email + '"' + '}';
-                $scope.paxData = ' "pax" : [';
+                $scope.paxData = ' "pax" : [ ';
 
                 // generate data
                 $scope.book.postData =' "token":"' + $scope.token + '",  "contact" :' + $scope.contactData + ',"lang":"' + $scope.language + '"';
@@ -189,12 +189,33 @@ app.controller('checkoutController', [
                         + '/' + ('0' + $scope.passengers[i].birth.date).slice(-2);
                     // passport expiry date
                     $scope.passengers[i].passport.expire.full = $scope.passengers[i].passport.expire.year + '/' + ('0' + (parseInt($scope.passengers[i].passport.expire.month) + 1)).slice(-2) + '/' + ('0' + $scope.passengers[i].passport.expire.date).slice(-2);
+
+                    //$scope.paxData = $scope.paxData + '{ "type":"' + $scope.passengers[i].type + '", "title":"' + $scope.passengers[i].title + '" , "name":"' + $scope.passengers[i].name + '" ';
+
+                    //if ($scope.birthDateRequired) {
+                    //    $scope.paxData = $scope.paxData + ', "dob":"' + $scope.passengers[i].birth.full + '" ,';
+                    //}
+
+                    //if ($scope.nationalityRequired) {
+                    //    $scope.paxData = $scope.paxData + ', "nationality":"' + $scope.passengers[i].nationality+ '" ,';
+                    //}
+
+                    //if ($scope.passportRequired) {
+                    //    $scope.paxData = $scope.paxData + ', "passportNo":"' + $scope.passengers[i].passport.number + '" , "passportExp":"' + $scope.passengers[i].passport.expire.full + '" , "passportCountry":"' + $scope.passengers[i].passport.country ;
+                    //}
+
+                    //if (i != $scope.passengers.length - 1) {
+                    //    $scope.paxData = $scope.paxData + '}' + ',';
+                    //} else {
+                    //    $scope.paxData = $scope.paxData + '}';
+                    //}
+
                     if (!$scope.passportRequired) {
                         if (i != $scope.passengers.length - 1) {
-                            $scope.paxData = $scope.paxData + '{ "type":"' + $scope.passengers[i].type + '", "title":"' + $scope.passengers[i].title + '" , "name":"' + $scope.passengers[i].name + '" , "dob":"' + $scope.passengers[i].birth.full + '" , "nationality":"' + $scope.passengers[i].passport.country + '" },';
+                            $scope.paxData = $scope.paxData + '{ "type":"' + $scope.passengers[i].type + '", "title":"' + $scope.passengers[i].title + '" , "name":"' + $scope.passengers[i].name + '" , "dob":"' + $scope.passengers[i].birth.full + '" , "nationality":"' + $scope.passengers[i].nationality+ '" },';
                         }
                         else {
-                            $scope.paxData = $scope.paxData + '{ "type":"' + $scope.passengers[i].type + '", "title":"' + $scope.passengers[i].title + '" , "name":"' + $scope.passengers[i].name + '" , "dob":"' + $scope.passengers[i].birth.full + '"  , "nationality":"' + $scope.passengers[i].passport.country + '" }';
+                            $scope.paxData = $scope.paxData + '{ "type":"' + $scope.passengers[i].type + '", "title":"' + $scope.passengers[i].title + '" , "name":"' + $scope.passengers[i].name + '" , "dob":"' + $scope.passengers[i].birth.full + '"  , "nationality":"' + $scope.passengers[i].nationality + '" }';
                         }
                     }
                     else
@@ -205,15 +226,15 @@ app.controller('checkoutController', [
                         else {
                             $scope.paxData = $scope.paxData + '{ "type":"' + $scope.passengers[i].type + '", "title":"' + $scope.passengers[i].title + '" , "name":"' + $scope.passengers[i].name + '" , "dob":"' + $scope.passengers[i].birth.full + '" , "passportNo":"' + $scope.passengers[i].passport.number + '" , "passportExp":"' + $scope.passengers[i].passport.expire.full + '" , "passportCountry":"' + $scope.passengers[i].passport.country + '" , "nationality":"' + $scope.passengers[i].passport.country + '" }';
                         }
-                    }
-
-                    
+                    }                 
                 }
+                
                 $scope.paxData = $scope.paxData + ']';
                 $scope.book.postData = '{' + $scope.book.postData + ',' + $scope.paxData + '}';
+                console.log($scope.book.postData);
                 $scope.book.postData = JSON.parse($scope.book.postData);
 
-                //console.log($scope.book.postData);
+                console.log($scope.book.postData);
 
                 //Check Authorization
                 var authAccess = getAuthAccess();
