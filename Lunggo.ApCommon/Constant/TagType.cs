88c47c27648158a07724a5079e6c41c5d5b7c906
Indera Aji Waskitho <indera.aji@travelmadezy.com>
2016-08-17 17:43:59 +0700

    Eticket Issued Notification

---
+++ Lunggo.ApCommon/Constant/TagType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lunggo.ApCommon.Constant
+{
+    public static class TagType
+    {
+        public const string DeviceId = "__deviceid";
+        public const string ClientId = "__clientid";
+        public const string Platform = "__platform";
+        public const string Version = "__version";
+        public const string Email = "__email";
+        public const string Phone = "__phone";
+        public const string Language = "__language";
+    }
+}
--- Lunggo.Framework/Notifications/NotificationClient.cs
+++ Lunggo.Framework/Notifications/NotificationClient.cs
@@ -6,12 +6,10 @@ namespace Lunggo.Framework.Notifications
     {
         internal abstract void Init(string connString, string hubName);
         internal abstract string RegisterDevice(string notificationHandle, string deviceId);
-        internal abstract bool SetTags(string registrationId, string notificationHandle, Platform platform,
-            Dictionary<string, string> tags);
-        internal abstract bool AddTags(string registrationId, string notificationHandle, Platform platform,
-            Dictionary<string, string> tags);
+        internal abstract bool UpdateTags(string registrationId, string notificationHandle, Platform platform,
+            Dictionary<string, string> newTags);
         internal abstract void DeleteRegistration(string registrationId);
-        internal abstract void PushNotification(Dictionary<string, string> tags, Notification notification);
-        internal abstract void PushSilentNotification(Dictionary<string, string> tags, object data);
+        internal abstract void PushNotification(Notification notification, Dictionary<string, string> tags);
+        internal abstract void PushSilentNotification(object data, Dictionary<string, string> tags);
     }
 }
