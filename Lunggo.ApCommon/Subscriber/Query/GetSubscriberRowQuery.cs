b052e485cc8c631c1bbea337a11da872fcbaba81
Mega Annisa <mega.annisa@travelmadezy.com>
2016-09-15 17:10:41 +0700

    fix

---
+++ Lunggo.ApCommon/Subscriber/Query/GetSubscriberRowQuery.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using Lunggo.Framework.Database;
+using Lunggo.Repository.TableRecord;
+
+namespace Lunggo.ApCommon.Subscriber.Query
+{
+    internal class GetSubscriberRowQuery : QueryBase<GetSubscriberRowQuery, SubscriberTableRecord>
+    {
+        protected override string GetQuery(dynamic condition = null)
+        {
+            var queryBuilder = new StringBuilder();
+            queryBuilder.Append(CreateSelectClause());
+            queryBuilder.Append(CreateWhereClause());
+            return queryBuilder.ToString();
+        }
+
+        private static string CreateSelectClause()
+        {
+            var clauseBuilder = new StringBuilder();
+            clauseBuilder.Append("SELECT * ");
+            clauseBuilder.Append("FROM Subscriber ");
+            return clauseBuilder.ToString();
+        }
+
+        private static string CreateWhereClause()
+        {
+            var clauseBuilder = new StringBuilder();
+            clauseBuilder.Append("WHERE HashLink = @HashLink");
+            return clauseBuilder.ToString();
+        }
+    }
+}
