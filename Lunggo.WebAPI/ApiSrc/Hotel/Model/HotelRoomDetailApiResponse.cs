d7c5001fcc2a46d6589b5d0de395a03696b98798
Intan Dea Yutami <intan.yutami@travelmadezy.com>
2016-10-05 09:41:57 +0700

    update files

---
+++ Lunggo.WebAPI/ApiSrc/Hotel/Model/HotelRoomDetailApiResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using Lunggo.ApCommon.Hotel.Model;
+using Newtonsoft.Json;
+
+namespace Lunggo.WebAPI.ApiSrc.Hotel.Model
+{
+    public class HotelRoomDetailApiResponse
+    {
+        [JsonProperty("roomDetails")]
+        public List<HotelRoom> RoomDetails { get; set; } 
+    }
+}
