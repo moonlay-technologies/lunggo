6319f2c5f4a811b7a20bffa9390a337095c807fe
Mega Annisa <mega.annisa@travelmadezy.com>
2016-10-24 14:07:13 +0700

    D : add custom checkbox jquery

---
+++ Lunggo.CustomerWeb/Assets/travorama20/js/HotelController.js
@@ -0,0 +1,25 @@
+﻿jQuery(document).ready(function ($) {
+    $('body .checkbox').on('click touchstart', function () {
+        var id = $(this).parent().find('.check');
+        if ($(id).is(':checked')) {
+            id.checked = false;
+            $(this).removeClass('active');
+        } else {
+            id.checked = true;
+            $(this).addClass('active');
+        }
+    });
+
+    $('body .radio').on('click touchstart', function () {
+        var id = $(this).parent().find('.check-radio');
+        $('body .radio').checked = false;
+        $('body .radio').removeClass('active');
+        if ($(id).is(':checked')) {
+            id.checked = false;
+            $(this).removeClass('active');
+        } else {
+            id.checked = true;
+            $(this).addClass('active');
+        }
+    });
+});
