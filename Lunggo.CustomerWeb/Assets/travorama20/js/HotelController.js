1bc0465573512897540334afd16f9764706d9a90
Mega Annisa <mega.annisa@travelmadezy.com>
2016-10-24 15:17:45 +0700

    D : Add jquery fix listing image

--- Lunggo.CustomerWeb/Assets/travorama20/js/HotelController.js
+++ Lunggo.CustomerWeb/Assets/travorama20/js/HotelController.js
@@ -22,4 +22,16 @@
             $(this).addClass('active');
         }
     });
+
+    $("body .img-list").each(function (i, elem) {
+        var img = $(elem);
+        var div = $("<div />").css({
+            background: "url(" + img.attr("src") + ") no-repeat",
+            width: "143px",
+            height: "180px",
+            "background-size": "cover",
+            "background-position": "center"
+    });
+        img.replaceWith(div);
+    });
 });
