c0a6e3887a69b28bcd968aaa81df80276f6832d7
Mega Annisa <mega.annisa@travelmadezy.com>
2016-11-02 12:11:54 +0700

    M : Add jQuery + update style

--- Lunggo.CustomerWeb/Assets/mobile/js/HotelController.js
+++ Lunggo.CustomerWeb/Assets/mobile/js/HotelController.js
@@ -48,25 +48,94 @@
 
         // **********
         // Custom Checkbox
-        $('body .switch').on('click touchstart', function () {
-            var id = $(this).parent().find('.check');
+        $('body .checkbox').on('click', function () {
+            var id = $(this).find('.check');
             if ($(id).is(':checked')) {
-                id.checked = false;
-                $(this).removeClass('active');
-            } else {
                 id.checked = true;
                 $(this).addClass('active');
+            } else {
+                id.checked = false;
+                $(this).removeClass('active');
             }
         });
 
         // **********
         // Custom Radio
-        $('body .radio').on('click touchstart', function () {
-            var id = $(this).parent().find('.check-radio');
-            $('body .radio').checked = false;
-            $('body .radio').removeClass('active');
+        $('body .round').on('click', function () {
+            var id = $(this).find('.check-radio');
+            $('body .round').checked = false;
+            $('body .round').removeClass('active');
             id.checked = true;
             $(this).addClass('active');
+        });
+
+        // **********
+        // Reset
+        $('body #reset').on('click', function () {
+            $('body #filter-hotel input').each(function (index, item) {
+                var type = $(item).attr('type');
+                var id = $(this).parent();
+                if (type == 'radio' || type == 'checkbox') {
+                    $(item).attr('checked', false);
+                    id.removeClass('active');
+                } else {
+                    $(item).val('');
+                }
+            });
+        });
+
+        // **********
+        // Open Detail Room
+        $('body .dh-list').on('click', function () {
+            $('body .dh-list-container').toggleClass('active');
+        });
+
+        // **********
+        // Counter Detail Room
+        $('body .btn-min').on('click', function(){
+            var value = $(this).parent().find('.result-room').val();
+            value = calculate(value, 'min');
+
+            $(this).parent().find('.result-room').val(value);
+        });
+
+        $('body .btn-plus').on('click', function(){
+            var value = $(this).parent().find('.result-room').val();
+            var limit = $(this).attr('attr-limit');
+            value = calculate(value, 'plus', limit);
+
+            $(this).parent().find('.result-room').val(value);
+        });
+
+        function calculate(value, type, limit){
+            value = parseInt(value);
+            limit = parseInt(limit);
+
+            if(value < 0){
+                value = 0;
+            }
+
+            if (type == 'min') {
+                if(value > 0){
+                    value = value - 1;
+                }
+            } else if (type == 'plus') {
+                value = value + 1;
+
+                if(value > limit){
+                    value = limit;
+                }
+            }
+
+            return value;
+        }
+
+        // **********
+        // Slick Slider Detail Hotel
+        $('.dh-slider').slick({
+            autoplay: true,
+            autoplaySpeed: 2500,
+            dots: false
         });
 
         // **********
