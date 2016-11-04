f55d8181757340cb822c8829d50ca4a3d06b7b14
Intan Dea Yutami <intan.yutami@travelmadezy.com>
2016-11-04 15:55:25 +0700

    Check if checkout and payment page has expiry handler, delete daysBetween function in js

--- Lunggo.CustomerWeb/Assets/travorama20/js/checkoutController.js
+++ Lunggo.CustomerWeb/Assets/travorama20/js/checkoutController.js
@@ -981,22 +981,6 @@ app.controller('confirmationController', [
             return hours + "j " + minutes + "m";
         }
 
-        function daysBetween(date1, date2) {
-
-            // The number of milliseconds in one day
-            var ONE_DAY = 1000 * 60 * 60 * 24
-
-            // Convert both dates to milliseconds
-            var date1_ms = date1.getTime()
-            var date2_ms = date2.getTime()
-
-            // Calculate the difference in milliseconds
-            var difference_ms = Math.abs(date1_ms - date2_ms)
-
-            // Convert back to days and return
-            return Math.round(difference_ms / ONE_DAY)
-        }
-
         $scope.capitalizeFirstLetter = function (sentence) {
             var words = sentence.split(" ");
             var text = "";
@@ -1072,22 +1056,6 @@ app.controller('thankyouController', [
                     console.log('Not Authorized');
                 }
             }, 15000);
-        }
-
-        function daysBetween(date1, date2) {
-
-            // The number of milliseconds in one day
-            var ONE_DAY = 1000 * 60 * 60 * 24
-
-            // Convert both dates to milliseconds
-            var date1_ms = date1.getTime()
-            var date2_ms = date2.getTime()
-
-            // Calculate the difference in milliseconds
-            var difference_ms = Math.abs(date1_ms - date2_ms)
-
-            // Convert back to days and return
-            return Math.round(difference_ms / ONE_DAY)
         }
 
         $scope.capitalizeFirstLetter = function (sentence) {
