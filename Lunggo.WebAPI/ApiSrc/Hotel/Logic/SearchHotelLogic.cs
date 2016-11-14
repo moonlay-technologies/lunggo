07ab5fe36c22e11be798556d33f896ed65d3f916
Mega Annisa <mega.annisa@travelmadezy.com>
2016-11-14 16:31:29 +0700

    D : doing bug sprint

--- Lunggo.WebAPI/ApiSrc/Hotel/Logic/SearchHotelLogic.cs
+++ Lunggo.WebAPI/ApiSrc/Hotel/Logic/SearchHotelLogic.cs
@@ -129,7 +129,6 @@ namespace Lunggo.WebAPI.ApiSrc.Hotel.Logic
                     Page = searchServiceResponse.Page,
                     PageCount = searchServiceResponse.PageCount,
                     PerPage = searchServiceResponse.PerPage,
-                    PageCount = searchServiceResponse.PageCount,
                     MaxPrice = searchServiceResponse.MaxPrice,
                     MinPrice = searchServiceResponse.MinPrice,
                     HotelFilterDisplayInfo = searchServiceResponse.HotelFilterDisplayInfo,
