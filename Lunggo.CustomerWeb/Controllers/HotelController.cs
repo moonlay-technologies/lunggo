440a3b7e6bf1ca8848723638004ab1494f6902ea
Intan Dea Yutami <intan.yutami@travelmadezy.com>
2016-11-28 16:35:54 +0700

    sementara

--- Lunggo.CustomerWeb/Controllers/HotelController.cs
+++ Lunggo.CustomerWeb/Controllers/HotelController.cs
@@ -14,112 +14,113 @@ namespace Lunggo.CustomerWeb.Controllers
     public class HotelController : Controller
     {
         // GET: Hotel
-        //public ActionResult Search()
-        //{
-        //    try
-        //    {
-        //        NameValueCollection query = Request.QueryString;
-        //        HotelSearchApiRequest model = new HotelSearchApiRequest(query);
-
-        //        return View(model);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest, ex.Message);
-        //    }
-
-        //}
         public ActionResult Search()
-        {
-            return View();
-            }
-        public ActionResult DetailHotel()
-            {
-            return View();
-            }
-        public ActionResult Checkout()
         {
             try
             {
-                NameValueCollection query = HttpUtility.ParseQueryString(searchParam);
+                NameValueCollection query = Request.QueryString;
                 HotelSearchApiRequest model = new HotelSearchApiRequest(query);
-                searchParam = model.SearchParam;
-                var searchParamObject = model.SearchParamObject;
 
-                return View(new { searchId, hotelCd, searchParam, searchParamObject });
+                return View(model);
             }
             catch (Exception ex)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest, ex.Message);
             }
+
         }
-        //public ActionResult DetailHotel(string searchId, int hotelCd, string searchParam)
+        //public ActionResult Search()
         //{
-        //    return View(new { searchId, hotelCd, searchParam });
+        //    return View();
+        //    }
+        //public ActionResult DetailHotel()
+        //    {
+        //    return View();
+        //    }
+        //public ActionResult Checkout()
+        //{
+        //    //try
+        //    //{
+        //    //    NameValueCollection query = HttpUtility.ParseQueryString(searchParam);
+        //    //    HotelSearchApiRequest model = new HotelSearchApiRequest(query);
+        //    //    searchParam = model.SearchParam;
+        //    //    var searchParamObject = model.SearchParamObject;
+
+        //    //    return View(new { searchId, hotelCd, searchParam, searchParamObject });
+        //    //}
+        //    //catch (Exception ex)
+        //    //{
+        //    //    return new HttpStatusCodeResult(HttpStatusCode.BadRequest, ex.Message);
+        //    //}
+        //    return View();
+        //}
+        public ActionResult DetailHotel(string searchId, int hotelCd, string searchParam)
+        {
+            return View(new { searchId, hotelCd, searchParam });
+        }
+
+        //public ActionResult Checkout()
+        //{
+        //    return View();
         //}
 
-        ////public ActionResult Checkout()
-        ////{
-        ////    return View();
-        ////}
+        [RequireHttps]
+        public ActionResult Checkout(string token)
+        {
+            var hotelDetail = HotelService.GetInstance().GetSelectionFromCache(token);
 
-        //[RequireHttps]
-        //public ActionResult Checkout(string token)
-        //{
-        //    var hotelDetail = HotelService.GetInstance().GetSelectionFromCache(token);
+            if (hotelDetail != null)
+            {
+                if (TempData["HotelCheckoutOrBookingError"] != null)
+                {
+                    ViewBag.Message = "BookFailed";
+                    return View();
 
-        //    if (hotelDetail != null)
-        //    {
-        //        if (TempData["HotelCheckoutOrBookingError"] != null)
-        //        {
-        //            ViewBag.Message = "BookFailed";
-        //            return View();
+                }
 
-        //        }
+                if (token == null)
+                {
+                    ViewBag.Message = "BookExpired";
+                    return View();
+                }
 
-        //        if (token == null)
-        //        {
-        //            ViewBag.Message = "BookExpired";
-        //            return View();
-        //        }
-           
-        //        try
-        //        {
-        //            var hotelService = HotelService.GetInstance();
-        //            var payment = PaymentService.GetInstance();
-        //            //var expiryTime = hotelService.GetSelectionExpiry(token);
-        //            //var savedPassengers = flight.GetSavedPassengers(User.Identity.GetEmail());
-        //            //var savedCreditCards = User.Identity.IsAuthenticated
-        //            //    ? payment.GetSavedCreditCards(User.Identity.GetEmail())
-        //            //    : new List<SavedCreditCard>();
-        //            return View(new HotelCheckoutData
-        //            {
-        //                Token = token,
-        //                HotelDetail = hotelDetail,
-        //                ExpiryTime = hotelService.GetSelectionExpiry(token).GetValueOrDefault(),
-        //                //SavedPassengers = savedPassengers,
-        //                //SavedCreditCards = savedCreditCards
-        //            });
-        //        }
-        //        catch
-        //        {
-        //            ViewBag.Message = "BookExpired";
-        //            return View(new HotelCheckoutData
-        //            {
-        //                Token = token
-        //            });
-        //        }
-        //    }
-        //    else
-        //    {
-        //        return RedirectToAction("Index", "UW000TopPage");
-        //    }
+                try
+                {
+                    var hotelService = HotelService.GetInstance();
+                    var payment = PaymentService.GetInstance();
+                    //var expiryTime = hotelService.GetSelectionExpiry(token);
+                    //var savedPassengers = flight.GetSavedPassengers(User.Identity.GetEmail());
+                    //var savedCreditCards = User.Identity.IsAuthenticated
+                    //    ? payment.GetSavedCreditCards(User.Identity.GetEmail())
+                    //    : new List<SavedCreditCard>();
+                    return View(new HotelCheckoutData
+                    {
+                        Token = token,
+                        HotelDetail = hotelDetail,
+                        ExpiryTime = hotelService.GetSelectionExpiry(token).GetValueOrDefault(),
+                        //SavedPassengers = savedPassengers,
+                        //SavedCreditCards = savedCreditCards
+                    });
+                }
+                catch
+                {
+                    ViewBag.Message = "BookExpired";
+                    return View(new HotelCheckoutData
+                    {
+                        Token = token
+                    });
+                }
+            }
+            else
+            {
+                return RedirectToAction("Index", "UW000TopPage");
+            }
 
-        //}
+        }
 
-        //[RequireHttps]
-        //[HttpPost]
-        //[ActionName("Checkout")]
+        [RequireHttps]
+        [HttpPost]
+        [ActionName("Checkout")]
         public ActionResult CheckoutPost(string rsvNo)
         {
             return RedirectToAction("Payment", "Payment", new { rsvNo });
