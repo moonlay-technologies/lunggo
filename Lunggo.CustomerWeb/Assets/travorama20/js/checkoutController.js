c00e0e9ed1eb4cf6f81b729ab1f93254fe49932d
Intan Dea Yutami <intan.yutami@travelmadezy.com>
2016-12-20 14:22:31 +0700

    buat controller confirmation js

--- Lunggo.CustomerWeb/Assets/travorama20/js/checkoutController.js
+++ Lunggo.CustomerWeb/Assets/travorama20/js/checkoutController.js
@@ -916,21 +916,3 @@ app.controller('checkoutController', [
     }
 ]);// checkout controller
 
-app.controller('confirmationController', [
-    '$http', '$scope', function ($http, $scope) {
-
-        $scope.pageLoaded = true;
-        $scope.msToTime = function (duration) {
-
-            var milliseconds = parseInt((duration % 1000) / 100),
-                 seconds = parseInt((duration / 1000) % 60),
-                 minutes = parseInt((duration / (1000 * 60)) % 60),
-                 hours = parseInt((duration / (1000 * 60 * 60)));
-            hours = hours;
-            minutes = minutes;
-            seconds = seconds;
-            return hours + "j " + minutes + "m";
-        }
-
-    }
-]);
