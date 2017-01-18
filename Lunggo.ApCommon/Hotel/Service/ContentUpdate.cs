9b203ccf75f8d6accc4e3ffe944197279f1ce9b1
Suheri Marpaung <suheri@travelmadezy.com>
2017-01-18 16:56:21 +0700

    add folder to blob image

--- Lunggo.ApCommon/Hotel/Service/ContentUpdate.cs
+++ Lunggo.ApCommon/Hotel/Service/ContentUpdate.cs
@@ -216,55 +216,64 @@ namespace Lunggo.ApCommon.Hotel.Service
 
         public void UpdateHotelImage()
         {
-            //Get hotelDetail
-            //Get hotelImage
-            var baseBiggerUrl = ConfigManager.GetInstance().GetConfigValue("hotel", "bigSizeImage");
-            
+            Dictionary<int,string> hotelbedsBaseUrl = new Dictionary<int,string>();
+            List<int> hotelCodeList = new List<int> {120819, 192558};
+            //List<string> folderList = new List<string> { "bigger", "standard" };
+            hotelbedsBaseUrl.Add(0, ConfigManager.GetInstance().GetConfigValue("hotel", "bigSizeImage"));
+            hotelbedsBaseUrl.Add(1, ConfigManager.GetInstance().GetConfigValue("hotel", "standardSizeImage"));
             var blobService = BlobStorageService.GetInstance();
-            for (var i = 1; i <= 600000; i++)
+            for (var i = 0; i <= 1; i++)
             {
                 try
                 {
-                    var hotel = GetHotelDetailFromTableStorage(i);
-                    Console.WriteLine("hotelCd: " + i);
+                    var hotel = GetHotelDetailFromTableStorage(hotelCodeList[i]);
+                    Console.WriteLine("hotelCd: " + hotelCodeList[i]);
+                    Debug.Print("hotelCd: " + hotelCodeList[i]);
                     if (hotel.ImageUrl != null)
                     {
                         using (WebClient webClient = new WebClient())
                         {
                             foreach (var url in hotel.ImageUrl)
                             {
-                                try
+                                foreach (var baseUrl in hotelbedsBaseUrl)
                                 {
-                                    var completedUrl = string.Concat(baseBiggerUrl, url.Path);
-                                    byte[] imageFile = webClient.DownloadData(completedUrl);
-                                    string fileName = url.Path.Split('/').LastOrDefault();
-                                    blobService.WriteFileToBlob(new BlobWriteDto
+                                    try
                                     {
-                                        FileBlobModel = new FileBlobModel
+                                        var imageType = baseUrl.Key == 0 ? "bigger" : "standard";
+                                        Debug.Print(imageType);
+                                        var completedUrl = string.Concat(baseUrl.Value, url.Path);
+                                        byte[] imageFile = webClient.DownloadData(completedUrl);
+                                        string fileName = url.Path.Split('/').LastOrDefault();
+                                        Debug.Print("URL : " + hotelCodeList[i] + "/" + imageType + "/" + url.Type + "/" + fileName);
+                                        blobService.WriteFileToBlob(new BlobWriteDto
                                         {
-                                            FileInfo = new FileInfo
+                                            FileBlobModel = new FileBlobModel
                                             {
-                                                FileName = i + "/" + url.Type + "/" +fileName,
-                                                ContentType = "image/jpeg",
-                                                FileData = imageFile
+                                                FileInfo = new FileInfo
+                                                {
+                                                    FileName = hotelCodeList[i] + "/" + imageType + "/" + url.Type + "/" + fileName,
+                                                    ContentType = "image/jpeg",
+                                                    FileData = imageFile
+                                                },
+                                                Container = "HotelImage"
                                             },
-                                            Container = "HotelImage"
-                                        },
-                                        SaveMethod = SaveMethod.Force
-                                    });
-                                }
-                                catch
-                                {
-                                    continue;
+                                            SaveMethod = SaveMethod.Force
+                                        });
+                                    }
+                                    catch
+                                    {
+                                        continue;
+                                    }
                                 }
                             }
                         }
                     }
-                    Console.WriteLine("Hotel Location saved for: " + i);
+                    Console.WriteLine("Hotel Image saved for: " + hotelCodeList[i]);
+                    Debug.Print("Hotel Image saved for: " + hotelCodeList[i]);
                 }
                 catch
                 {
-                    Console.WriteLine("Hotel with code: " + i + " not found");
+                    Console.WriteLine("Hotel with code: " + hotelCodeList[i] + " not found");
                 }
             }
         }
