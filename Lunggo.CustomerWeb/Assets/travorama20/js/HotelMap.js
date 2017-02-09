4cf43183c5a6f57aeb97703d50d2a0eaf1d05eb4
Mega Annisa <mega.annisa@travelmadezy.com>
2017-02-09 12:13:09 +0700

    D + M : fix jquery

--- Lunggo.CustomerWeb/Assets/travorama20/js/HotelMap.js
+++ Lunggo.CustomerWeb/Assets/travorama20/js/HotelMap.js
@@ -106,7 +106,7 @@ function initMap() {
     var otherHotel = { lat: -8.711247, lng: 115.181347 };
 
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 15,
+        zoom: 17,
         center: currentHotel,
         mapTypeControl: false,
         streetViewControl: false,
@@ -151,7 +151,7 @@ function initMap() {
             '<div class="orange-txt sm-txt underline-txt">Rp ' + '999.999.999' + '</div>' +
             '<div class="orange-txt bold-txt md-txt"><sup>Rp </sup>' + '999.999.999' + '</div>' +
             '</div>' +
-            '<div class="map-button">' + '<a class="btn btn-yellow sm-btn"' + 'href="' + 'www.travorama.com' + '"' + '>PESAN</a>' + '</div>' +
+            '<div class="map-button">' + '<a class="btn btn-yellow sm-btn"' + 'href="' + '#' + '"' + '>PESAN</a>' + '</div>' +
             '</div>' +
             '<div class="map-price sm-txt semibold-txt">' + 'Rp ' + '999.999.999' + '</div>' +
             '</div>';
@@ -161,59 +161,21 @@ function initMap() {
             maxWidth: 350,
         });
 
-        //var infoPrice = '<div class="map-price sm-txt semibold-txt">' + 'Rp ' + '999.999.999' + '</div>';
-
-        //var infoOpenPrice = new google.maps.InfoWindow({
-        //    content: infoPrice
-        //});
-
         infoWindow.open(map, marker);
 
-        //infoOpenPrice.addListener('click', function () {
-        //    $('.gm-style-iw').css('border', '1px solid red');
-        //});
-       
-
-        //marker.addListener('click', function () {
-        //    infoWindow.open(map, marker);
-        //    infoOpenPrice.close();
-        //});
-
-        //map.addListener('click', function () {
-        //    infoWindow.close();
-        //});
-
-        
-
-        //infoOpenPrice.addListener('domready', function () {
-        //    var iwOuter = $('.gm-style-iw');
-        //    iwOuter.addClass('gm-price');
-
-        //    var iwBackground = iwOuter.prev();
-        //    iwBackground.children().css({ 'display': 'none' });
-
-        //    var iwCloseBtn = iwOuter.next();
-        //    iwCloseBtn.css({ display: 'none' });
-
-        //    $('.gm-price').click(function () {
-        //        infoWindow.open(map, marker);
-        //        var p = $(this).parent().parent().children(':nth-child(4)').siblings();
-        //        var q = $(this).parent().parent().children(':nth-child(4)');
-        //        p.children('.gm-style-iw').hide();
-        //        //q.children(':nth-child(2)').show();
-        //    });
-        //});
-
         infoWindow.addListener('domready', function () {
-            var iwOuter = $('.gm-style-iw');
+            var iwOuter = $('.gm-style-iw').addClass('custom');
 
             $('.map-container').hide();
 
-            $('.map-price').click(function() {
-                $(this).parent().find('.map-container').show();
-                var p = $(this).closest('.gm-style').find('.gm-style-iw').parent().parent().css('background', 'red');
-                p.find('.gm-style-iw').siblings().css('background', 'green');
-                $(this).hide();
+            $('.map-price').click(function () {
+                var p = $(this).closest('.gm-style-iw').parent();
+                var q = p.siblings();
+
+                p.find('.map-container').show();
+                p.find('.map-price').hide();
+                q.find('.map-container').hide();
+                q.find('.map-price').show();
             });
 
             var iwBackground = iwOuter.prev();
