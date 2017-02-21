1a2cc005279771e4731f2bf576110d83c3ded4d5
Mega Annisa <mega.annisa@travelmadezy.com>
2017-02-21 18:35:17 +0700

    Travorama b2b

---
+++ Lunggo.CustomerWeb/Controllers/B2BTemplateController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lunggo.CustomerWeb.Controllers
+{
+    public class B2BTemplateController : Controller
+    {
+        // GET: B2BTemplate
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult SearchFlight()
+        {
+            return View();
+        }
+        public ActionResult SearchHotel()
+        {
+            return View();
+        }
+        public ActionResult OrderListFlight()
+        {
+            return View();
+        }
+        public ActionResult OrderListHotel()
+        {
+            return View();
+        }
+
+    }
+}
