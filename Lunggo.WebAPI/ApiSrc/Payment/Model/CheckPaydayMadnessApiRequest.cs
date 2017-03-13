5c853d87d0241ec8535a4b9c12eaf3a0a14c0d72
Intan Dea Yutami <intan.yutami@travelmadezy.com>
2017-03-13 16:55:11 +0700

    file ketinggalan

---
+++ Lunggo.WebAPI/ApiSrc/Payment/Model/CheckPaydayMadnessApiRequest.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace Lunggo.WebAPI.ApiSrc.Payment.Model
+{
+    public class CheckPaydayMadnessApiRequest
+    {
+        [JsonProperty("voucherCode")]
+        public string VoucherCode { get; set; }
+        [JsonProperty("rsvNo")]
+        public string RsvNo { get; set; }
+    }
+}
---
+++ Lunggo.WebAPI/ApiSrc/Payment/Model/CheckPaydayMadnessApiResponse.cs
@@ -0,0 +1,17 @@
+﻿using Lunggo.WebAPI.ApiSrc.Common.Model;
+using Newtonsoft.Json;
+
+namespace Lunggo.WebAPI.ApiSrc.Payment.Model
+{
+    public class CheckPaydayMadnessResponse : ApiResponseBase
+    {
+        [JsonProperty("amount", NullValueHandling = NullValueHandling.Ignore)]
+        public decimal? DiscountAmount { get; set; }
+        [JsonProperty("name", NullValueHandling = NullValueHandling.Ignore)]
+        public string DiscountName { get; set; }
+        [JsonProperty("isAvailable", NullValueHandling = NullValueHandling.Ignore)]
+        public bool? IsAvailable { get; set; }
+        [JsonProperty("replaceOriginalDiscount", NullValueHandling = NullValueHandling.Ignore)]
+        public bool? ReplaceOriginalDiscount { get; set; }
+    }
+}
