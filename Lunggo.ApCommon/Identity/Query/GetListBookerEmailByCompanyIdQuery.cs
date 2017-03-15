449d429f67e82194ccb7d35f0898c2f1be2dfcb2
Intan Dea Yutami <intan.yutami@travelmadezy.com>
2017-03-15 14:46:22 +0700

    file ketinggalan

---
+++ Lunggo.ApCommon/Identity/Query/GetListBookerEmailByCompanyIdQuery.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using Lunggo.Framework.Database;
+
+namespace Lunggo.ApCommon.Identity.Query
+{
+    public class GetListBookerEmailByCompanyIdQuery : DbQueryBase<GetListBookerEmailByCompanyIdQuery, string>
+    {
+        private GetListBookerEmailByCompanyIdQuery()
+        {
+
+        }
+
+        protected override string GetQuery(dynamic condition = null)
+        {
+            var queryBuilder = new StringBuilder();
+            queryBuilder.Append("SELECT [User].Email " +
+                                "FROM [User] " +
+                                "LEFT JOIN [UserRole] ON [User].Id = UserRole.UserId " +
+                                "LEFT JOIN [Role] ON UserRole.RoleId = [Role].Id " +
+                                "WHERE [Role].Name = 'Booker' AND [User].CompanyId = @CompanyId");
+            return queryBuilder.ToString();
+        }
+    }
+}
