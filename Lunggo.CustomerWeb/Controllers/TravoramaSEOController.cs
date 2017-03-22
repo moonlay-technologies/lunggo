452444550724c1d0462cf854363e68e31b454254
Mega Annisa <mega.annisa@travelmadezy.com>
2017-03-22 17:21:34 +0700

    seo page

---
+++ Lunggo.CustomerWeb/Controllers/TravoramaSEOController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lunggo.CustomerWeb.Controllers
+{
+    public class TravoramaSEOController : Controller
+    {
+        // GET: TravoramaSEO
+        public ActionResult HotelSEO()
+        {
+            return View();
+        }
+        public ActionResult AirlineSEO()
+        {
+            return View();
+        }
+    }
+}
