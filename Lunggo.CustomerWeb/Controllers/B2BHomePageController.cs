1a3b1b1d7af8efca390a2f8af2625914ab597cd6
Mega Annisa <mega.annisa@travelmadezy.com>
2017-04-03 17:15:55 +0700

    D + M : B2B homepage

---
+++ Lunggo.CustomerWeb/Controllers/B2BHomePageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lunggo.CustomerWeb.Controllers
+{
+    public class B2BHomePageController : Controller
+    {
+        // GET: B2BHomePage
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
