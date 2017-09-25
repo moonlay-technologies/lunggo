b14bfc1a54a7fe5841df910cf47de24aa9be5904
carloswirama <vincentius_carlos@yahoo.co.id>
2017-09-25 19:26:27 +0700

    Promo Hore Gajian minus banners

--- Lunggo.CustomerWeb/Controllers/PromoController.cs
+++ Lunggo.CustomerWeb/Controllers/PromoController.cs
@@ -92,6 +92,40 @@ namespace Lunggo.CustomerWeb.Controllers
 
                     return View("PaydayMadnessPermataBank");
 
+                case "horegajian":
+
+                    ViewBag.Title = "Hore Gajian";
+                    ViewBag.ImageSrcDesktop = "/Assets/images/campaign/2017HoreGajian/landingpage_desktop.jpg";
+                    ViewBag.ImageSrcMobile = "/Assets/images/campaign/2017HoreGajian/landingpage_mobile.jpg";
+                    ViewBag.ImageAlt = "Hore Gajian";
+                    ViewBag.PromoTitle = "Hore Gajian";
+                    ViewBag.PromoDescription = "";
+                    ViewBag.KodePromo = "";
+                    ViewBag.PeriodeBooking = "8 - 24 September 2017";
+                    ViewBag.PeriodeInap = "Kapan Saja";
+
+                    howToGet.Add("Cari dan pesan kamar hotel melalui situs Travorama");
+                    howToGet.Add("Isi data pemesan dan detail tamu");
+                    howToGet.Add("Pada halaman metode pembayaran, pilih metode pembayaran yang akan digunakan");
+                    howToGet.Add("Masukkan kode promo dan klik gunakan, harga akan otomatis terpotong sesuai promo");
+                    howToGet.Add("Selesaikan transaksi pembayaran");
+                    howToGet.Add("E-Voucher hotel dikirimkan ke kamu");
+                    ViewBag.HowToGet = howToGet;
+
+                    termList.Add("Diskon sebesar 9% (Maks. Rp 1.000.000) dan minimal transaksi Rp 500.000");
+                    termList.Add("Berlaku untuk semua hotel di dalam dan luar negeri");
+                    termList.Add("Berlaku untuk pemesanan kamar hotel melalui situs Travorama ");
+                    termList.Add("Kode promo dapat digunakan berkali-kali selama periode promo berlangsung");
+                    termList.Add("Berlaku untuk semua metode pembayaran");
+                    termList.Add("Promo ini tidak dapat digabungkan dengan promo lain ");
+                    termList.Add("Periode booking: 8 - 24 September 2017");
+                    termList.Add("Periode inap: kapan saja");
+                    termList.Add("Perhitungan refund dilakukan berdasarkan nominal yang dibayar setelah dipotong diskon, bukan harga awal.");
+                    termList.Add("Travorama berhak penuh untuk mengubah syarat dan ketentuan tanpa pemberitahuan terlebih dahulu.");
+                    ViewBag.Terms = termList;
+
+                    return View("Promo");
+
                 case "septemberseru":
 
                     ViewBag.Title = "September Seru";
