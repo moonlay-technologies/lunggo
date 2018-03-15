6eb1b98575983dc822f79963c22ac6b9832f7038
badiali2 <badi.alinugraha@gmail.com>
2018-03-15 10:23:46 +0700

    tambah controller register

---
+++ Lunggo.CustomerWeb/Controllers/RegisterController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lunggo.CustomerWeb.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public ActionResult Register(string text)
+        {
+            return View(model: text);
+        }
+    }
+}
