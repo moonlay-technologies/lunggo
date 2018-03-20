42b767f804f05ecd1d59b13a068ae8be3cf7ae62
Indera Aji Waskitho <indera.aji@travelmadezy.com>
2018-03-20 15:03:22 +0700

    WIP commit verify phone

---
+++ Lunggo.WebAPI/ApiSrc/Account/Logic/VerifyPhoneLogic.cs
@@ -0,0 +1,81 @@
+﻿using Lunggo.ApCommon.Account.Model.Logic;
+using Lunggo.ApCommon.Account.Service;
+using Lunggo.WebAPI.ApiSrc.Account.Model;
+using Lunggo.WebAPI.ApiSrc.Common.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace Lunggo.WebAPI.ApiSrc.Account.Logic
+{
+    public static partial class AccountLogic
+    {
+        public static ApiResponseBase VerifyPhone(VerifyPhoneApiRequest apiRequest)
+        {
+            if (!string.IsNullOrEmpty(apiRequest.PhoneNumber))
+            {
+                if (apiRequest.PhoneNumber.StartsWith("0"))
+                {
+                    apiRequest.PhoneNumber = apiRequest.PhoneNumber.Substring(1);
+                }
+
+                if (AccountService.GetInstance().CheckPhoneNumberFormat(apiRequest.PhoneNumber) == false)
+                {
+                    return new ApiResponseBase
+                    {
+                        StatusCode = HttpStatusCode.BadRequest,
+                        ErrorCode = "ERR_INVALID_FORMAT_PHONENUMBER"
+                    };
+                }               
+            }
+
+            else
+            {
+                return new ApiResponseBase
+                {
+                    StatusCode = HttpStatusCode.BadRequest,
+                    ErrorCode = "ERR_INVALID_REQUEST"
+                };
+            }
+
+            var input = PreProcess(apiRequest);
+            if (AccountService.GetInstance().CheckExpireTime(input) == false)
+            {
+                return new ApiResponseBase
+                {
+                    StatusCode = HttpStatusCode.BadRequest,
+                    ErrorCode = "ERR_OTP_EXPIRED"
+                };
+            }
+
+            if (AccountService.GetInstance().CheckOtp(input) == false)
+            {
+                return new ApiResponseBase
+                {
+                    StatusCode = HttpStatusCode.BadRequest,
+                    ErrorCode = "ERR_OTP_NOT_VALID"
+                };
+            }
+            return new ApiResponseBase
+            {
+                StatusCode = HttpStatusCode.OK
+            };
+
+        }
+
+
+        public static CheckOtpInput PreProcess(CheckOtpApiRequest apiRequest)
+        {
+            return new CheckOtpInput
+            {
+                PhoneNumber = apiRequest.PhoneNumber,
+                Email = apiRequest.Email,
+                Otp = apiRequest.Otp
+            };
+        }
+
+
+    }
+}
---
+++ Lunggo.WebAPI/ApiSrc/Account/Model/VerifyPhoneApiRequest.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lunggo.WebAPI.ApiSrc.Account.Model
+{
+    public class VerifyPhoneApiRequest
+    {
+        [JsonProperty("otp", NullValueHandling = NullValueHandling.Ignore)]
+        public string Otp { get; set; }
+        [JsonProperty("phoneNumber", NullValueHandling = NullValueHandling.Ignore)]
+        public string PhoneNumber { get; set; }
+    }
+}
