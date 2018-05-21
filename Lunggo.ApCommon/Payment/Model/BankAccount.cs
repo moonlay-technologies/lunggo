5abbc7664f1874f457f7ce291ba25afa562cd433
Waski <indera.aji@travelmadezy.com>
2018-05-21 16:18:54 +0700

    fix bankaccount json

--- Lunggo.ApCommon/Payment/Model/BankAccount.cs
+++ Lunggo.ApCommon/Payment/Model/BankAccount.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Lunggo.ApCommon.Payment.Model
 {
     public class BankAccount
     {
+        [JsonProperty("accountNumber", NullValueHandling = NullValueHandling.Ignore)]
         public string AccountNumber { get; set; }
+        [JsonProperty("bankName", NullValueHandling = NullValueHandling.Ignore)]
         public string BankName { get; set; }
+        [JsonProperty("branch", NullValueHandling = NullValueHandling.Ignore)]
         public string Branch { get; set; }
+        [JsonProperty("ownerName", NullValueHandling = NullValueHandling.Ignore)]
         public string OwnerName { get; set; }
     }
 }
